5f838a46245e96f4fdf9b4ce167abf78a80787ec
406743395 <406743395@qq.com>
2015-06-02 10:09:39 +0800

    das

--- src/com/member/service/systemmanager/parameter/controller/ParameterController.java
+++ src/com/member/service/systemmanager/parameter/controller/ParameterController.java
@@ -1,6 +1,7 @@
 package com.member.service.systemmanager.parameter.controller;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.member.service.systemmanager.parameter.bean.ParameterBean;
 import com.member.service.systemmanager.parameter.po.SystemParameter;
 import com.member.service.systemmanager.parameter.service.ParameterService;
 
@@ -22,7 +24,7 @@ public class ParameterController {
 	
 	
 	@RequestMapping(value = "/show")
-	public ModelAndView login(HttpServletRequest request,
+	public ModelAndView show(HttpServletRequest request,
 			HttpServletResponse response)
 			 {
 		// 带参数重定向  view.setViewName("redirect:/index{id}");
@@ -48,6 +50,57 @@ public class ParameterController {
 			  e.printStackTrace();
 			  try {
 					response.sendRedirect(basePath+"login.jsp");
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		return null;
+
+	}
+	
+	@RequestMapping(value = "/set")
+	public ModelAndView set(HttpServletRequest request,
+			HttpServletResponse response, String dayCount, String goldFlg,
+			String goldMax, String goldMin, String goldTake, String scoreMax,
+			String scoreMin, String scoreTake, String glbMax, String glbMin,
+			String glbTake, String scoreInMin, String scoreInTake) {
+		// 带参数重定向 view.setViewName("redirect:/index{id}");
+		String basePath = "/";
+		// System.out.println(a);
+		ModelAndView mv = new ModelAndView();
+		try {
+			request.setCharacterEncoding("utf-8");
+			response.setCharacterEncoding("utf-8");
+			String path = request.getContextPath();
+			basePath = request.getScheme() + "://" + request.getServerName()
+					+ ":" + request.getServerPort() + path + "/";
+			SystemParameter systemParameter = parameterService
+					.getSystemParameter();
+			if(systemParameter == null){
+				systemParameter = new SystemParameter();
+				systemParameter.setSystemData("system");
+				systemParameter.setCreateTime(new Date());
+			}
+			ParameterBean pb = new ParameterBean();
+			pb.setDayCount(dayCount);
+			pb.setGoldFlg(goldFlg);
+			pb.setGoldMax(goldMax);
+			pb.setGoldMin(goldMin);
+			pb.setGoldTake(goldTake);
+			pb.setScoreMax(scoreMax);
+			pb.setScoreMin(scoreMin);
+			pb.setScoreTake(scoreTake);
+			pb.setGlbMin(glbMin);
+			pb.setGlbTake(glbTake);
+			pb.setGlbMax(glbMax);
+			pb.setScoreInTake(scoreInTake);
+			pb.setScoreInMin(scoreInMin);
+			parameterService.setSystemParameter(systemParameter, pb);
+			return mv;
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				response.sendRedirect(basePath + "login.jsp");
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
