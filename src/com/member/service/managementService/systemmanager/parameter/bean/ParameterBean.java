f325ff463092ac4c04888f2914830430d44b5481
TXB <1026800485@qq.com>
2015-06-02 11:23:11 +0800

    更新

---
+++ src/com/member/service/managementService/systemmanager/parameter/bean/ParameterBean.java
@@ -0,0 +1,98 @@
+package com.member.service.systemmanager.parameter.bean;
+
+public class ParameterBean {
+
+	private String dayCount;
+	private String goldFlg;
+	private String goldMax;
+	private String goldMin;
+	private String goldTake;
+	private String scoreMax;
+	private String scoreMin;
+	private String scoreTake;
+	private String glbMax;
+	private String glbMin;
+	private String glbTake;
+	private String scoreInMin;
+	private String scoreInTake;
+	public String getDayCount() {
+		return dayCount;
+	}
+	public void setDayCount(String dayCount) {
+		this.dayCount = dayCount;
+	}
+	public String getGoldFlg() {
+		return goldFlg;
+	}
+	public void setGoldFlg(String goldFlg) {
+		this.goldFlg = goldFlg;
+	}
+	public String getGoldMax() {
+		return goldMax;
+	}
+	public void setGoldMax(String goldMax) {
+		this.goldMax = goldMax;
+	}
+	public String getGoldMin() {
+		return goldMin;
+	}
+	public void setGoldMin(String goldMin) {
+		this.goldMin = goldMin;
+	}
+	public String getGoldTake() {
+		return goldTake;
+	}
+	public void setGoldTake(String goldTake) {
+		this.goldTake = goldTake;
+	}
+	public String getScoreMax() {
+		return scoreMax;
+	}
+	public void setScoreMax(String scoreMax) {
+		this.scoreMax = scoreMax;
+	}
+	public String getScoreMin() {
+		return scoreMin;
+	}
+	public void setScoreMin(String scoreMin) {
+		this.scoreMin = scoreMin;
+	}
+	public String getScoreTake() {
+		return scoreTake;
+	}
+	public void setScoreTake(String scoreTake) {
+		this.scoreTake = scoreTake;
+	}
+	public String getGlbMax() {
+		return glbMax;
+	}
+	public void setGlbMax(String glbMax) {
+		this.glbMax = glbMax;
+	}
+	public String getGlbMin() {
+		return glbMin;
+	}
+	public void setGlbMin(String glbMin) {
+		this.glbMin = glbMin;
+	}
+	public String getGlbTake() {
+		return glbTake;
+	}
+	public void setGlbTake(String glbTake) {
+		this.glbTake = glbTake;
+	}
+	public String getScoreInMin() {
+		return scoreInMin;
+	}
+	public void setScoreInMin(String scoreInMin) {
+		this.scoreInMin = scoreInMin;
+	}
+	public String getScoreInTake() {
+		return scoreInTake;
+	}
+	public void setScoreInTake(String scoreInTake) {
+		this.scoreInTake = scoreInTake;
+	}
+	
+	
+}
