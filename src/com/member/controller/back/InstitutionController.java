9e41da81928b294bbc1665dcd215810f28caf251
406743395 <406743395@qq.com>
2015-06-03 18:19:59 +0800

    dsd

--- src/com/member/controller/back/InstitutionController.java
+++ src/com/member/controller/back/InstitutionController.java
@@ -1,6 +1,7 @@
 package com.member.controller.back;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.member.beans.back.ParameterBean;
 import com.member.entity.Institution;
+import com.member.entity.SystemParameter;
 import com.member.services.back.InstitutionService;
 
 @Controller
@@ -54,4 +57,39 @@ public class InstitutionController {
 		return null;
 
 	}
+	
+	
+	@RequestMapping(value = "/set")
+	public ModelAndView set(HttpServletRequest request,
+			HttpServletResponse response) {
+		// 带参数重定向 view.setViewName("redirect:/index{id}");
+		String basePath = "/";
+		// System.out.println(a);
+		ModelAndView mv = new ModelAndView();
+		try {
+			request.setCharacterEncoding("utf-8");
+			response.setCharacterEncoding("utf-8");
+			String path = request.getContextPath();
+			basePath = request.getScheme() + "://" + request.getServerName()
+					+ ":" + request.getServerPort() + path + "/";
+			Institution institution = institutionService.getInstitutionInfo();
+			if(institution == null){
+				institution = new Institution();
+				institution.setSystemData("system");
+				institution.setCreateTime(new Date());
+			}
+			institutionService.setInstitution(institution);
+			return mv;
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				response.sendRedirect(basePath + "login.jsp");
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		return null;
+
+	}
+	
 }
