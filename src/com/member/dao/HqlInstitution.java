6dd50026a34600f5972b26b6512dccf316a0624e
406743395 <406743395@qq.com>
2015-06-03 16:11:24 +0800

    修改BUG

---
+++ src/com/member/dao/HqlInstitution.java
@@ -0,0 +1,7 @@
+package com.member.dao;
+
+public class HqlInstitution {
+
+	public final static String getInstitutionInfo=
+		"from Institution t where t.systemData='system'";
+}
---
+++ src/com/member/dao/impl/InstitutionDaoImpl.java
@@ -0,0 +1,11 @@
+package com.member.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.member.dao.InstitutionDao;
+import com.member.helper.dao.impl.BaseDaoImpl;
+
+@Component("InstitutionDaoImpl")
+public class InstitutionDaoImpl extends BaseDaoImpl implements InstitutionDao{
+
+}
