4832de61a51964e0862aaa93b49f63d8bc1b2012
406743395 <406743395@qq.com>
2015-06-04 21:34:32 +0800

    fsdfsd

--- src/com/member/dao/impl/ServiceManagerDaoImpl.java
+++ src/com/member/dao/impl/ServiceManagerDaoImpl.java
@@ -1,8 +1,10 @@
 package com.member.dao.impl;
 
+import org.springframework.stereotype.Component;
+
 import com.member.dao.ServiceManagerDao;
 import com.member.helper.dao.impl.BaseDaoImpl;
-
+@Component("ServiceManagerDaoImpl")
 public class ServiceManagerDaoImpl extends BaseDaoImpl implements
 		ServiceManagerDao {
 
