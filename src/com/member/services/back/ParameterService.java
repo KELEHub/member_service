5d2557bc7b99cd199e29fd741d3598f53865f7e2
406743395 <406743395@qq.com>
2015-06-05 22:15:41 +0800

    fsdfsd

--- src/com/member/services/back/ParameterService.java
+++ src/com/member/services/back/ParameterService.java
@@ -1,10 +1,9 @@
 package com.member.services.back;
 
-import com.member.beans.back.ParameterBean;
 import com.member.entity.SystemParameter;
 
 public interface ParameterService {
 	public SystemParameter getSystemParameter();
 	
-	public boolean setSystemParameter(SystemParameter systemParameter,ParameterBean pb);
+	public boolean setSystemParameter(SystemParameter systemParameter);
 }
