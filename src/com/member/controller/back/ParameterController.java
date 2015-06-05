a169cc78b1b1fcfc16fa4770882764a9a802aaf9
406743395 <406743395@qq.com>
2015-06-05 22:42:49 +0800

    fsdfsd

--- src/com/member/controller/back/ParameterController.java
+++ src/com/member/controller/back/ParameterController.java
@@ -19,67 +19,65 @@ import com.member.services.back.ParameterService;
 @Controller
 @RequestMapping(value = "/ParameterController")
 public class ParameterController {
-	
+
 	@Resource(name = "ParameterServiceImpl")
 	public ParameterService parameterService;
-	
-	
-	@RequestMapping(value = "/show",method = RequestMethod.POST)
-	public String show(Model model)
-			 {
+
+	@RequestMapping(value = "/show", method = RequestMethod.POST)
+	public String show(Model model) {
 		try {
-			 SystemParameter systemParameter = parameterService.getSystemParameter();
-			 if(systemParameter!=null){
-				 model.addAttribute("bean", systemParameter);
-				 return "back/parameter/systemParameter";
-			}else{
+			SystemParameter systemParameter = parameterService
+					.getSystemParameter();
+			if (systemParameter != null) {
+				model.addAttribute("bean", systemParameter);
+				return "back/parameter/systemParameter";
+			} else {
 				return "back/parameter/systemParameter";
 			}
-		
+
 		} catch (Exception e) {
-			  e.printStackTrace();
-			  return "back/parameter/systemParameter";
+			e.printStackTrace();
+			return "back/parameter/systemParameter";
 		}
 
 	}
-	
-	@RequestMapping(value = "/set",method = RequestMethod.POST)
+
+	@RequestMapping(value = "/set", method = RequestMethod.POST)
 	@ResponseBody
-	public BaseResult<Void> set(@RequestBody ParameterForm form,Model model) {
+	public BaseResult<Void> set(@RequestBody ParameterForm form, Model model) {
 		BaseResult<Void> result = new BaseResult<Void>();
 		try {
 			SystemParameter systemParameter = parameterService
 					.getSystemParameter();
-			if(systemParameter == null){
+			if (systemParameter == null) {
 				systemParameter = new SystemParameter();
 				systemParameter.setSystemData("system");
 				systemParameter.setCreateTime(new Date());
 			}
 			systemParameter.setDayCount(Integer.valueOf(form.getDayCount()));
 			systemParameter.setGoldFlg(form.getGoldFlg());
-			systemParameter.setGoldMax(Integer.valueOf(form.getGoldMax()));
-			systemParameter.setGoldMin(Integer.valueOf(form.getGoldMin()));
-			systemParameter.setGoldTake(Integer.valueOf(form.getGoldTake()));
-			systemParameter.setScoreMax(Integer.valueOf(form.getScoreMax()));
-			systemParameter.setScoreMin(Integer.valueOf(form.getScoreMin()));
-			systemParameter.setScoreTake(Integer.valueOf(form.getScoreTake()));
-			systemParameter.setGlbMin(Integer.valueOf(form.getGlbMin()));
-			systemParameter.setGlbTake(Integer.valueOf(form.getGlbTake()));
-			systemParameter.setScoreInTake(Integer.valueOf(form.getScoreInTake()));
-			systemParameter.setScoreInMin(Integer.valueOf(form.getScoreInMin()));
-			
+			systemParameter.setGoldMax(Long.valueOf(form.getGoldMax()));
+			systemParameter.setGoldMin(Long.valueOf(form.getGoldMin()));
+			systemParameter.setGoldTake(Long.valueOf(form.getGoldTake()));
+			systemParameter.setScoreMax(Long.valueOf(form.getScoreMax()));
+			systemParameter.setScoreMin(Long.valueOf(form.getScoreMin()));
+			systemParameter.setScoreTake(Long.valueOf(form.getScoreTake()));
+			systemParameter.setGlbMin(Long.valueOf(form.getGlbMin()));
+			systemParameter.setGlbTake(Long.valueOf(form.getGlbTake()));
+			systemParameter.setScoreInTake(Long.valueOf(form.getScoreInTake()));
+			systemParameter.setScoreInMin(Long.valueOf(form.getScoreInMin()));
+
 			parameterService.setSystemParameter(systemParameter);
 			result.setMsg("设置成功");
 			result.setSuccess(true);
 			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
-			result.setMsg("设置失败");
-			result.setSuccess(false);
+			result.setMsg("设置失败,数值设置不正确");
+			result.setSuccess(true);
 			return result;
 		}
 
 	}
-	
 
 }
