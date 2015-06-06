c92768bcce3d7d3e14681b9277ef8ef82ff53868
406743395 <406743395@qq.com>
2015-06-06 12:31:22 +0800

    fsdfsd

---
+++ src/com/member/form/back/InstitutionForm.java
@@ -0,0 +1,72 @@
+package com.member.form.back;
+
+
+public class InstitutionForm {
+
+	/**注册资金 */
+	private String registerGold;
+	/**直推礼包数*/
+	private String preCount;
+	/**5份礼包第一期*/
+	private String preaFirst;
+	/**5份礼包第二期*/
+	private String preaSecond;
+	/**5份礼包第三期*/
+	private String preaThree;
+	/**5份礼包第四期*/
+	private String preaFour;
+	/**5份礼包第5期*/
+	private String preaFive;
+	/**服务费*/
+	private String serviceCash;
+	public String getRegisterGold() {
+		return registerGold;
+	}
+	public void setRegisterGold(String registerGold) {
+		this.registerGold = registerGold;
+	}
+	public String getPreCount() {
+		return preCount;
+	}
+	public void setPreCount(String preCount) {
+		this.preCount = preCount;
+	}
+	public String getPreaFirst() {
+		return preaFirst;
+	}
+	public void setPreaFirst(String preaFirst) {
+		this.preaFirst = preaFirst;
+	}
+	public String getPreaSecond() {
+		return preaSecond;
+	}
+	public void setPreaSecond(String preaSecond) {
+		this.preaSecond = preaSecond;
+	}
+	public String getPreaThree() {
+		return preaThree;
+	}
+	public void setPreaThree(String preaThree) {
+		this.preaThree = preaThree;
+	}
+	public String getPreaFour() {
+		return preaFour;
+	}
+	public void setPreaFour(String preaFour) {
+		this.preaFour = preaFour;
+	}
+	public String getPreaFive() {
+		return preaFive;
+	}
+	public void setPreaFive(String preaFive) {
+		this.preaFive = preaFive;
+	}
+	public String getServiceCash() {
+		return serviceCash;
+	}
+	public void setServiceCash(String serviceCash) {
+		this.serviceCash = serviceCash;
+	}
+
+	
+}
