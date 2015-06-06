2e260db717ea874d9200848ea13c2c6c89f185aa
406743395 <406743395@qq.com>
2015-06-06 21:05:44 +0800

    fsdfsd

---
+++ src/com/member/util/CommonUtil.java
@@ -0,0 +1,25 @@
+package com.member.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CommonUtil {
+	
+	/**
+	 * 星期日---1
+	 * 星期一---2
+	 * 星期二---3
+	 * 星期三---4
+	 * 星期四---5
+	 * 星期五---6
+	 * 星期六---7
+	 * @return
+	 */
+	public static int getWeek(){
+		  Calendar c = Calendar.getInstance();
+		  c.setTime(new Date(System.currentTimeMillis()));
+		  int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+		  return dayOfWeek;
+	}
+
+}
