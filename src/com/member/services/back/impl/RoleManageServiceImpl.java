4689066a88d3fa9b72cfef8990354ecfa0cb860f
406743395 <406743395@qq.com>
2015-06-07 15:22:55 +0800

    fsdfsd

--- src/com/member/services/back/impl/RoleManageServiceImpl.java
+++ src/com/member/services/back/impl/RoleManageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.member.services.back.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.member.dao.ManageRoleDao;
 import com.member.entity.ManageRole;
+import com.member.entity.ManageUserRoleHub;
 import com.member.form.back.RoleForm;
 import com.member.services.back.RoleManageService;
 
@@ -26,10 +28,42 @@ public class RoleManageServiceImpl implements RoleManageService {
 		manageRoleDao.save(role);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
+	@Transactional(readOnly = true)
 	public List<ManageRole> getRoleList() {
 		String hql="select mr from ManageRole mr";
 		List result = manageRoleDao.queryByHql(hql);
 		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(readOnly = true)
+	public ManageRole getRoleById(Integer userId) {
+		String hql="select mr from ManageRole mr ,ManageUserRoleHub t where mr.id=t.roleId and t.userId=?";
+		List arguments = new ArrayList();
+		arguments.add(userId);
+	   List<ManageRole> result = (List<ManageRole>)manageRoleDao.queryByHql(hql,arguments);
+	   if (result != null && result.size() > 0) {
+			return  result.get(0);
+		} else {
+			return null;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(readOnly = true)
+	public ManageUserRoleHub getManageUserRoleHubById(Integer userId) {
+		String hql="from ManageUserRoleHub t where t.userId=?";
+		List arguments = new ArrayList();
+		arguments.add(userId);
+	   List<ManageUserRoleHub> result = (List<ManageUserRoleHub>)manageRoleDao.queryByHql(hql,arguments);
+	   if (result != null && result.size() > 0) {
+			return  result.get(0);
+		} else {
+			return null;
+		}
 	}
 }
