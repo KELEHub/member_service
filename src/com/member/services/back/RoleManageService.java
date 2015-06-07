a7a8fb834533c046f2a50c70f0d7d165d95d41e1
406743395 <406743395@qq.com>
2015-06-07 15:23:41 +0800

    fsdfsd

--- src/com/member/services/back/RoleManageService.java
+++ src/com/member/services/back/RoleManageService.java
@@ -3,9 +3,12 @@ package com.member.services.back;
 import java.util.List;
 
 import com.member.entity.ManageRole;
+import com.member.entity.ManageUserRoleHub;
 import com.member.form.back.RoleForm;
 
 public interface RoleManageService {
 	public void insertRole(RoleForm form);
 	public List<ManageRole> getRoleList();
+	public ManageRole getRoleById(Integer userId);
+	public ManageUserRoleHub getManageUserRoleHubById(Integer userId);
 }
