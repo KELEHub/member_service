13f3f8b35db231e98064bdb3f97888723a46f4c8
406743395 <406743395@qq.com>
2015-06-07 22:26:22 +0800

    fsdfsd

---
+++ src/com/member/form/back/EditeHistoryForm.java
@@ -0,0 +1,35 @@
+package com.member.form.back;
+
+public class EditeHistoryForm {
+	
+	private String numeber;
+	private String createDate;
+	private String remaind;
+	private String oprationMan;
+	public String getNumeber() {
+		return numeber;
+	}
+	public void setNumeber(String numeber) {
+		this.numeber = numeber;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getRemaind() {
+		return remaind;
+	}
+	public void setRemaind(String remaind) {
+		this.remaind = remaind;
+	}
+	public String getOprationMan() {
+		return oprationMan;
+	}
+	public void setOprationMan(String oprationMan) {
+		this.oprationMan = oprationMan;
+	}
+	
+
+}
---
+++ src/com/member/form/back/UserEditeForm.java
@@ -0,0 +1,58 @@
+package com.member.form.back;
+
+public class UserEditeForm {
+	
+	private String number;
+	private String userNumber;
+	private String crmMoney;
+	private String crmAccumulative;
+	private String shoppingMoney;
+	private String shoppingAccumulative;
+	private String error;
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getUserNumber() {
+		return userNumber;
+	}
+	public void setUserNumber(String userNumber) {
+		this.userNumber = userNumber;
+	}
+	public String getCrmMoney() {
+		return crmMoney;
+	}
+	public void setCrmMoney(String crmMoney) {
+		this.crmMoney = crmMoney;
+	}
+	public String getCrmAccumulative() {
+		return crmAccumulative;
+	}
+	public void setCrmAccumulative(String crmAccumulative) {
+		this.crmAccumulative = crmAccumulative;
+	}
+	public String getShoppingMoney() {
+		return shoppingMoney;
+	}
+	public void setShoppingMoney(String shoppingMoney) {
+		this.shoppingMoney = shoppingMoney;
+	}
+	public String getShoppingAccumulative() {
+		return shoppingAccumulative;
+	}
+	public void setShoppingAccumulative(String shoppingAccumulative) {
+		this.shoppingAccumulative = shoppingAccumulative;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	
+	
+
+}
