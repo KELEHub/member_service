4b8c37301a304ffe1b5f55cc96542d956b883cee
406743395 <406743395@qq.com>
2015-06-07 15:24:18 +0800

    fsdfsd

--- WebRoot/resources/back/common.js
+++ WebRoot/resources/back/common.js
@@ -161,6 +161,39 @@ function saveLimeteDeclaration(sFormId){
 	}
 }
 
+
+function saveCreateUser(sFormId){
+	var result = ajaxRequestForFormGetJson(sFormId);
+	if(result.success){
+		alert(result.msg);
+		$("#content-header").find("form[id='searchUserForm']").each(function(){
+				var formid = this.id;
+				ajaxRequestForFormGetJsp(formid);
+		});
+	}
+}
+
+function deleteUser(username){
+	 event.returnValue = confirm("删除是不可恢复的，你确认要删除吗？");
+	 if(event.returnValue){
+		 var reqObj={};
+	   reqObj["username"]=username;
+      var result = ajaxRequestForJsonGetJson("/CreateUserController/delete.do", reqObj);
+	  if(result.success){
+		alert(result.msg);
+		$("#content-header").find("form[id='searchUserForm']").each(function(){
+				var formid = this.id;
+				ajaxRequestForFormGetJsp(formid);
+		});
+	}
+	 }
+	
+}
+
+
+
+
+
 function showDynamicDialog(reqUrl,reqObj,dialogId){
 	var bathPath=$("#basePath").val();
 	var reqData={};
