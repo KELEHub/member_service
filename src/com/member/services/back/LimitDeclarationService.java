75084aa24b75423bde69767b8f881c54d57ea9cd
406743395 <406743395@qq.com>
2015-06-07 09:29:33 +0800

    fsdfsd

---
+++ src/com/member/services/back/LimitDeclarationService.java
@@ -0,0 +1,18 @@
+package com.member.services.back;
+
+import java.util.Date;
+
+import com.member.beans.back.enumData.ProjectEnum;
+import com.member.entity.LimitDeclaration;
+
+public interface LimitDeclarationService {
+	
+	public LimitDeclaration getLimitDeclarationInfo();
+	
+	public int getCountThisWeek(Date date,ProjectEnum project);
+	
+	public int getCountAll(ProjectEnum project);
+	
+	public boolean saveOrUpdate(Object ob);
+
+}
