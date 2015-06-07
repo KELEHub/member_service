9761d6610362f99aceeb40e31daa8e8a7e985b92
406743395 <406743395@qq.com>
2015-06-07 15:23:20 +0800

    fsdfsd

--- src/com/member/services/back/NmUserService.java
+++ src/com/member/services/back/NmUserService.java
@@ -3,9 +3,12 @@ package com.member.services.back;
 import java.util.List;
 
 import com.member.beans.back.MenuBean;
+import com.member.entity.NmsUser;
 
  public interface NmUserService {
 	 
 	 public List<MenuBean> getMenuByUserId(Integer userId);
+	 
+	 public List<NmsUser> getNmsUserAll();
 
 }
