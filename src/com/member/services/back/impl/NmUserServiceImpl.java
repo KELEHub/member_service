875625a84c9d4ea41bf3ea27a7c51c46e6babbf3
406743395 <406743395@qq.com>
2015-06-07 15:22:44 +0800

    fsdfsd

--- src/com/member/services/back/impl/NmUserServiceImpl.java
+++ src/com/member/services/back/impl/NmUserServiceImpl.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.member.beans.back.MenuBean;
+import com.member.dao.HqlUserRole;
 import com.member.dao.NmsUserDao;
+import com.member.entity.NmsUser;
 import com.member.services.back.NmUserService;
 
 @Service("NmUserServiceImpl")
@@ -19,8 +21,17 @@ public class NmUserServiceImpl implements NmUserService{
 	private NmsUserDao nmsUserDao;
 	
 	@Override
+	@Transactional(readOnly = true)
 	public List<MenuBean> getMenuByUserId(Integer userId) {
 		return nmsUserDao.getMenuByUserId(userId);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(readOnly = true)
+	public List<NmsUser> getNmsUserAll() {
+		List<NmsUser> list = (List<NmsUser>)nmsUserDao.queryByHql(HqlUserRole.getNmsUserAll);
+		return list;
 	}
 	
 
