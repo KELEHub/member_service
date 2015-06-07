9ac1271b073ac18f13694e2711992183b3c974c8
406743395 <406743395@qq.com>
2015-06-07 09:27:28 +0800

    fsdfsd

---
+++ src/com/member/beans/back/enumData/KindDataEnum.java
@@ -0,0 +1,9 @@
+package com.member.beans.back.enumData;
+
+public enum KindDataEnum {
+	/** 葛粮币*/
+	goldmoney,
+	/** 积分*/
+	points;
+
+}
---
+++ src/com/member/beans/back/enumData/ProjectEnum.java
@@ -0,0 +1,20 @@
+package com.member.beans.back.enumData;
+
+public enum ProjectEnum {
+	/** 会员转账*/
+	tootherman,
+	/** 充值*/
+	recharge,
+	/** 后台调整*/
+	frombank,
+	/** 积分转葛粮币增加*/
+	frompointsadd,
+	/** 单个服务积分*/
+	servicepointsforone,
+	/** 极差服务积分*/
+	servicepointsformuch,
+	/** 积分转葛粮币减少*/
+	togoldmoneycut,
+	/** 礼包发放*/
+	fromgifts;
+}
