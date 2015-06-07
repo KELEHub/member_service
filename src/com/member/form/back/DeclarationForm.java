b6edb0c9e41fd7450cd1942d73b693f1c9a47a0e
406743395 <406743395@qq.com>
2015-06-07 09:29:08 +0800

    fsdfsd

---
+++ src/com/member/form/back/DeclarationForm.java
@@ -0,0 +1,30 @@
+package com.member.form.back;
+
+public class DeclarationForm {
+	
+	private String declarationIndex;
+	private String maxDeclaration;
+	private String declarationAll;
+	public String getDeclarationIndex() {
+		return declarationIndex;
+	}
+	public void setDeclarationIndex(String declarationIndex) {
+		this.declarationIndex = declarationIndex;
+	}
+	public String getMaxDeclaration() {
+		return maxDeclaration;
+	}
+	public void setMaxDeclaration(String maxDeclaration) {
+		this.maxDeclaration = maxDeclaration;
+	}
+	public String getDeclarationAll() {
+		return declarationAll;
+	}
+	public void setDeclarationAll(String declarationAll) {
+		this.declarationAll = declarationAll;
+	}
+	
+	
+	
+
+}
