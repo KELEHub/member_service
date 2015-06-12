7533bf9e1a312b20bfddf2bbecec315a5a2a22b8
406743395 <406743395@qq.com>
2015-06-12 23:29:18 +0800

    fsdfsd

--- WebRoot/resources/back/common.js
+++ WebRoot/resources/back/common.js
@@ -689,3 +689,24 @@ function rechargeService(sFormId){
 function rechargeToForm(arg_id){
 	document.getElementById("serviceInfo_serviceId").value = arg_id;
 }
+
+  //格式化金额
+//优化负数格式化问题
+function fmoney(s, n) {
+	n = n > 0 && n <= 20 ? n : 2;
+	s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
+	var l = s.split(".")[0].split("").reverse(), r = s.split(".")[1];
+	t = "";
+	for (i = 0; i < l.length; i++) {
+		t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
+	}
+	return t.split("").reverse().join("") + "." + r;
+}
+
+function formatter(o, blur) {
+	if(o.value==""){
+		o.value = "0.00"
+		return;
+	}
+	o.value = fmoney(o.value,2);
+}
