c94e51215d33df967b95932acedfbcf259434234
406743395 <406743395@qq.com>
2015-06-13 17:10:59 +0800

    fsdfsd

--- src/com/member/dao/impl/IntegralManagerDaoImpl.java
+++ src/com/member/dao/impl/IntegralManagerDaoImpl.java
@@ -1,8 +1,10 @@
 package com.member.dao.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.member.dao.IntegralManagerDao;
 import com.member.helper.dao.impl.BaseDaoImpl;
-
+@Service("IntegralManagerDaoImpl")
 public class IntegralManagerDaoImpl extends BaseDaoImpl implements
 		IntegralManagerDao {
 
