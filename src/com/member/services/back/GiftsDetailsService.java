5ad36e2b34ba096856dd45541a8f676841f54fed
406743395 <406743395@qq.com>
2015-06-14 21:13:59 +0800

    fsdfsd

---
+++ src/com/member/services/back/GiftsDetailsService.java
@@ -0,0 +1,15 @@
+package com.member.services.back;
+
+import java.util.List;
+
+import com.member.beans.back.enumData.BatchNoEnum;
+import com.member.entity.SendGiftsDetails;
+
+public interface GiftsDetailsService {
+	
+	public List<SendGiftsDetails> getGiftsDetailsList(Integer countNumber,BatchNoEnum batchNo);
+	
+	public String getCountGoldAll(Integer countNumber,BatchNoEnum batchNo);
+	
+
+}
