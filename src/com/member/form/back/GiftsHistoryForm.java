c3c8201f0ebde2556d8f14b8bc39728af58151a2
406743395 <406743395@qq.com>
2015-06-14 11:14:52 +0800

    fsdfsd

---
+++ src/com/member/form/back/GiftsHistoryForm.java
@@ -0,0 +1,43 @@
+package com.member.form.back;
+
+public class GiftsHistoryForm {
+	
+	private String numeber;
+	private String createDate;
+	private String remaind;
+	private String oprationMan;
+	private String name;
+	public String getNumeber() {
+		return numeber;
+	}
+	public void setNumeber(String numeber) {
+		this.numeber = numeber;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getRemaind() {
+		return remaind;
+	}
+	public void setRemaind(String remaind) {
+		this.remaind = remaind;
+	}
+	public String getOprationMan() {
+		return oprationMan;
+	}
+	public void setOprationMan(String oprationMan) {
+		this.oprationMan = oprationMan;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
