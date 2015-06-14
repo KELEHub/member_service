282b2c89a60f46792606f9de92fe27de44768c8b
406743395 <406743395@qq.com>
2015-06-14 22:37:03 +0800

    fsdfsd

--- src/com/member/services/back/impl/GiftsDetailsServiceImpl.java
+++ src/com/member/services/back/impl/GiftsDetailsServiceImpl.java
@@ -1,23 +1,40 @@
 package com.member.services.back.impl;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.member.beans.back.enumData.BatchNoEnum;
+import com.member.beans.back.enumData.KindDataEnum;
+import com.member.beans.back.enumData.ProjectEnum;
 import com.member.dao.GiftsDao;
+import com.member.entity.AccountDetails;
+import com.member.entity.GiftsDetails;
+import com.member.entity.Information;
 import com.member.entity.SendGiftsDetails;
 import com.member.services.back.GiftsDetailsService;
+import com.member.services.back.InformationService;
+import com.member.services.back.ParameterService;
+import com.member.util.CommonUtil;
 @Service("GiftsDetailsServiceImpl")
 public class GiftsDetailsServiceImpl implements GiftsDetailsService{
 
 	
 	@Resource(name = "GiftsDaoImpl")
 	private GiftsDao giftsDao;
+	
+	@Resource(name = "InformationServiceImpl")
+	public InformationService informationService;
+	
+	@Resource(name = "ParameterServiceImpl")
+	public ParameterService parameterService;
 
 	@SuppressWarnings("unchecked")
 	@Transactional(readOnly = true)
@@ -48,5 +65,43 @@ public class GiftsDetailsServiceImpl implements GiftsDetailsService{
 		}
 		
 		return null;
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void senGold(SendGiftsDetails ss) {
+	
+		Information	information = informationService.getInformationByName(ss.getNumber());
+		GiftsDetails giftsDetails = parameterService.getGiftsDetailsById(ss.getGiftsDetailsId());
+		BigDecimal shoppingMoney = information.getShoppingMoney().add(new BigDecimal(ss.getGoldMoney()));
+		BigDecimal shoppingAccumulative = information.getShoppingAccumulative().add(new BigDecimal(ss.getGoldMoney()));
+		giftsDetails.setPointNumber(giftsDetails.getPointNumber()+1);
+		information.setShoppingMoney(shoppingMoney);
+		information.setShoppingAccumulative(shoppingAccumulative);
+		AccountDetails shopingDetails = new AccountDetails();
+		shopingDetails.setUserNumber(ss.getNumber());
+		shopingDetails.setCreateTime(new Date());
+		shopingDetails.setKindData(KindDataEnum.points);
+		/** 日期类别统计 */
+		shopingDetails.setDateNumber(ss.getDateNumber());
+		shopingDetails.setProject(ProjectEnum.fromgifts);
+		/** 积分余额 */
+		shopingDetails.setPointbalance(shoppingMoney);
+		/** 葛粮币余额 */
+		shopingDetails.setGoldmoneybalance(information.getCrmMoney());
+		/** 收入 */
+		shopingDetails.setIncome(new BigDecimal(ss.getGoldMoney()));
+		/** 支出 */
+		shopingDetails.setPay(new BigDecimal(0));
+
+		/** 备注 */
+		shopingDetails.setRedmin("礼包释放");
+		/** 用户ID */
+		shopingDetails.setUserId(information.getId());
+		giftsDao.saveOrUpdate(information);
+		giftsDao.saveOrUpdate(giftsDetails);
+		giftsDao.saveOrUpdate(shopingDetails);
+		giftsDao.delete(ss);
+		
 	}
 }
