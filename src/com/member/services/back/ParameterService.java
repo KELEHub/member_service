22af8bc4a6696ac9af689943c6726aaa9008d773
406743395 <406743395@qq.com>
2015-06-14 11:15:25 +0800

    fsdfsd

--- src/com/member/services/back/ParameterService.java
+++ src/com/member/services/back/ParameterService.java
@@ -1,10 +1,26 @@
 package com.member.services.back;
 
+import java.util.List;
+
+import com.member.entity.GiftsDetails;
+import com.member.entity.GiftsHistory;
 import com.member.entity.SystemParameter;
 
 public interface ParameterService {
 	public SystemParameter getSystemParameter();
 	
 	public boolean setSystemParameter(SystemParameter systemParameter);
+	
+	public List<GiftsDetails> getGiftsDetails();
+	
+	public GiftsDetails getGiftsDetailsById(Integer id);
+	
+	public List<GiftsDetails> getGiftsDetailsByNumber(String number);
+	
+	public List<GiftsHistory> getGiftsHistoryAll();
+	
+	public List<GiftsHistory> getGiftsHistoryByOperationId(Integer id);
+	
+	public void saveOrUpdate(GiftsDetails gd,GiftsHistory gh);
 	
 }
