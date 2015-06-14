f69f436ce84db1e71492ee1233e22ae8dd37f699
406743395 <406743395@qq.com>
2015-06-14 23:33:43 +0800

    fsdfsd

---
+++ src/com/member/form/back/IntegralHistoryForm.java
@@ -0,0 +1,49 @@
+package com.member.form.back;
+
+public class IntegralHistoryForm {
+	
+	private String userNumber;
+	private String project;
+	private String income;
+	private String pay;
+	private String pointbalance;
+	private String createTime;
+	public String getUserNumber() {
+		return userNumber;
+	}
+	public void setUserNumber(String userNumber) {
+		this.userNumber = userNumber;
+	}
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+	public String getIncome() {
+		return income;
+	}
+	public void setIncome(String income) {
+		this.income = income;
+	}
+	public String getPay() {
+		return pay;
+	}
+	public void setPay(String pay) {
+		this.pay = pay;
+	}
+	public String getPointbalance() {
+		return pointbalance;
+	}
+	public void setPointbalance(String pointbalance) {
+		this.pointbalance = pointbalance;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	
+
+}
