6317440c24876b9803cbc41b3457aa6a249eb9c3
406743395 <406743395@qq.com>
2015-06-14 11:15:07 +0800

    fsdfsd

---
+++ src/com/member/form/back/GiftsForm.java
@@ -0,0 +1,60 @@
+package com.member.form.back;
+
+public class GiftsForm {
+	
+	private String number;
+	private String name;
+	private String createTime;
+	private String remaind;
+	private String typeGift;
+	private String id;
+	private String pointNumber;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getRemaind() {
+		return remaind;
+	}
+	public void setRemaind(String remaind) {
+		this.remaind = remaind;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getTypeGift() {
+		return typeGift;
+	}
+	public void setTypeGift(String typeGift) {
+		this.typeGift = typeGift;
+	}
+	public String getPointNumber() {
+		return pointNumber;
+	}
+	public void setPointNumber(String pointNumber) {
+		this.pointNumber = pointNumber;
+	}
+	
+	
+	
+	
+
+}
