8064a52af025be034dbcc6d49acaae3d554ebbaf
406743395 <406743395@qq.com>
2015-06-14 11:15:53 +0800

    fsdfsd

---
+++ src/com/member/services/back/impl/InformationServiceImpl.java
@@ -0,0 +1,52 @@
+package com.member.services.back.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.member.dao.InstitutionDao;
+import com.member.entity.Information;
+import com.member.services.back.InformationService;
+
+@Service("InformationServiceImpl")
+public class InformationServiceImpl implements InformationService{
+	
+	
+	@Resource(name = "InstitutionDaoImpl")
+    InstitutionDao institutionDao;
+	
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(readOnly = true)
+	public Information getInformationById(Integer id) {
+		String hql="from Information mr where mr.id=?";
+		List arguments = new ArrayList();
+		arguments.add(id);
+		List<Information> result = (List<Information>)institutionDao.queryByHql(hql,arguments);
+		if(result!=null && result.size()>0){
+			return result.get(0);
+		}
+		return null;
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(readOnly = true)
+	public Information getInformationByName(String number) {
+		String hql="from Information mr where mr.number=?";
+		List arguments = new ArrayList();
+		arguments.add(number);
+		List<Information> result = (List<Information>)institutionDao.queryByHql(hql,arguments);
+		if(result!=null && result.size()>0){
+			return result.get(0);
+		}
+		return null;
+	}
+
+}
