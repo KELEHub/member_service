e2053383979ca893a5e545ea9960b83f87ef5f0c
406743395 <406743395@qq.com>
2015-06-14 23:34:02 +0800

    fsdfsd

--- src/com/member/services/back/impl/IntegralManagerServiceImpl.java
+++ src/com/member/services/back/impl/IntegralManagerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.member.services.back.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -7,6 +8,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.member.beans.back.enumData.ProjectEnum;
 import com.member.dao.HqlIntegralManager;
 import com.member.dao.IntegralManagerDao;
 import com.member.entity.AccountDetails;
@@ -40,5 +42,32 @@ public class IntegralManagerServiceImpl implements IntegralManagerService {
 	public List<RepeatedMoneyStatistics> getAvailableRangeIntegral() {
 		return (List<RepeatedMoneyStatistics>) integralManagerDao.queryByHql(
 				HqlIntegralManager.getIntegralHistoryByUserName);
+	}
+
+	@Override
+	public List<AccountDetails> getIntegralHistoryPoints() {
+		String hql = "from AccountDetails t where t.project=? or t.project=?";
+		List arguments = new ArrayList();
+		arguments.add(ProjectEnum.fromgifts);
+		arguments.add(ProjectEnum.servicepointsforone);
+		List<AccountDetails> result = (List<AccountDetails>)integralManagerDao.queryByHql(hql,arguments);
+		if(result!=null && result.size()>0){
+			return result;
+		}
+		return null;
+	}
+
+	@Override
+	public List<AccountDetails> getIntegralHistoryPointsByNumber(String number) {
+		String hql = "from AccountDetails t where (t.project=? or t.project=?) and t.userNumber=?";
+		List arguments = new ArrayList();
+		arguments.add(ProjectEnum.fromgifts);
+		arguments.add(ProjectEnum.servicepointsforone);
+		arguments.add(number);
+		List<AccountDetails> result = (List<AccountDetails>)integralManagerDao.queryByHql(hql,arguments);
+		if(result!=null && result.size()>0){
+			return result;
+		}
+		return null;
 	}
 }
