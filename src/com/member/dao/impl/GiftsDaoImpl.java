fd907ce90f6cbf8b37198413cde894a71aeb4060
406743395 <406743395@qq.com>
2015-06-14 21:12:14 +0800

    fsdfsd

---
+++ src/com/member/dao/impl/GiftsDaoImpl.java
@@ -0,0 +1,10 @@
+package com.member.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.member.dao.GiftsDao;
+import com.member.helper.dao.impl.BaseDaoImpl;
+@Component("GiftsDaoImpl")
+public class GiftsDaoImpl extends BaseDaoImpl implements GiftsDao{
+
+}
