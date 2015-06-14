5c26fac1b17a0a3f98a801274ec9b2fd87de23f9
406743395 <406743395@qq.com>
2015-06-14 23:33:20 +0800

    fsdfsd

--- src/com/member/controller/back/IntegralManagerController.java
+++ src/com/member/controller/back/IntegralManagerController.java
@@ -1,5 +1,6 @@
 package com.member.controller.back;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -9,12 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.member.beans.back.enumData.ProjectEnum;
 import com.member.entity.AccountDetails;
 import com.member.entity.RepeatedMoneyStatistics;
-import com.member.form.back.UserEditeForm;
-import com.member.helper.BaseResult;
+import com.member.form.back.IntegralHistoryForm;
 import com.member.services.back.IntegralManagerService;
 
 @Controller
@@ -26,7 +26,24 @@ public class IntegralManagerController {
 
 	@RequestMapping(value = {"/showIntegralHistory","/queryStatistics"},method = RequestMethod.POST)
 	public String showIntegralHistory(Model model){
-		List<AccountDetails> result = integralManagerService.getIntegralHistory();
+		List<AccountDetails> list = integralManagerService.getIntegralHistoryPoints();
+		List<IntegralHistoryForm> result = new ArrayList<IntegralHistoryForm>();
+		if(list!=null &&list.size()>0){
+			for(AccountDetails ad : list){
+				IntegralHistoryForm form =new IntegralHistoryForm();
+				form.setCreateTime(ad.getCreateTime().toString());
+				form.setIncome(ad.getIncome().toString());
+				form.setPay(ad.getPay().toString());
+				form.setPointbalance(ad.getPointbalance().toString());
+				form.setUserNumber(ad.getUserNumber());
+				if(ad.getProject().equals(ProjectEnum.servicepointsformuch)){
+					form.setProject("极差服务积分");
+				}else{
+					form.setProject("礼包释放积分");
+				}
+				result.add(form);
+			}
+		}
 		model.addAttribute("result", result);
 		return "back/integralManager/integralIssueHistory";
 	}
@@ -36,6 +53,40 @@ public class IntegralManagerController {
 		List<RepeatedMoneyStatistics> result = integralManagerService.getAvailableRangeIntegral();
 		model.addAttribute("result", result);
 		return "back/integralManager/rangeIssue";
+	}
+	
+	
+	@RequestMapping(value = "/serch", method = RequestMethod.POST)
+	public String serch(@RequestBody IntegralHistoryForm form, Model model) {
+		try {
+
+			List<AccountDetails> list  = integralManagerService.getIntegralHistoryPointsByNumber(form
+					.getUserNumber());
+			List<IntegralHistoryForm> result = new ArrayList<IntegralHistoryForm>();
+			if(list!=null &&list.size()>0){
+				for(AccountDetails ad : list){
+					IntegralHistoryForm ifForm =new IntegralHistoryForm();
+					ifForm.setCreateTime(ad.getCreateTime().toString());
+					ifForm.setIncome(ad.getIncome().toString());
+					ifForm.setPay(ad.getPay().toString());
+					ifForm.setPointbalance(ad.getPointbalance().toString());
+					ifForm.setUserNumber(ad.getUserNumber());
+					if(ad.getProject().equals(ProjectEnum.servicepointsformuch)){
+						form.setProject("极差服务积分");
+					}else{
+						form.setProject("礼包释放积分");
+					}
+					result.add(ifForm);
+				}
+			}
+
+			model.addAttribute("result", result);
+			return "back/integralManager/integralIssueHistory";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "back/integralManager/integralIssueHistory";
+		}
+
 	}
 	
 //	@RequestMapping(value = "/queryStatistics",method = RequestMethod.POST)
