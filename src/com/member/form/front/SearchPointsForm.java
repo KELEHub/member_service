583be3dff91480b194c8ce6807ed5dcde8d5c5fc
406743395 <406743395@qq.com>
2015-06-21 03:28:49 +0800

    fsdfsd

---
+++ src/com/member/form/front/SearchPointsForm.java
@@ -0,0 +1,56 @@
+package com.member.form.front;
+
+public class SearchPointsForm {
+
+	private String goldFlg;
+	
+	private String yearFlg;
+	
+	private String monthFlg;
+	
+	private String projectFlg;
+	
+	private String error;
+
+	public String getGoldFlg() {
+		return goldFlg;
+	}
+
+	public void setGoldFlg(String goldFlg) {
+		this.goldFlg = goldFlg;
+	}
+
+	public String getYearFlg() {
+		return yearFlg;
+	}
+
+	public void setYearFlg(String yearFlg) {
+		this.yearFlg = yearFlg;
+	}
+
+	public String getMonthFlg() {
+		return monthFlg;
+	}
+
+	public void setMonthFlg(String monthFlg) {
+		this.monthFlg = monthFlg;
+	}
+
+	public String getProjectFlg() {
+		return projectFlg;
+	}
+
+	public void setProjectFlg(String projectFlg) {
+		this.projectFlg = projectFlg;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	
+}
