a0545ecb3bc4e8236233b148a905a2243642ed61
406743395 <406743395@qq.com>
2015-06-21 03:27:50 +0800

    fsdfsd

---
+++ src/com/member/beans/front/AccountBean.java
@@ -0,0 +1,65 @@
+package com.member.beans.front;
+
+public class AccountBean {
+	private String kindData;
+	private String createTime;
+	private String project;
+	private String income;
+	private String pay;
+	private String pointbalance;
+	private String goldmoneybalance;
+	private String redmin;
+	
+	public String getKindData() {
+		return kindData;
+	}
+	public void setKindData(String kindData) {
+		this.kindData = kindData;
+	}
+	
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+	public String getIncome() {
+		return income;
+	}
+	public void setIncome(String income) {
+		this.income = income;
+	}
+	public String getPay() {
+		return pay;
+	}
+	public void setPay(String pay) {
+		this.pay = pay;
+	}
+	public String getPointbalance() {
+		return pointbalance;
+	}
+	public void setPointbalance(String pointbalance) {
+		this.pointbalance = pointbalance;
+	}
+	public String getGoldmoneybalance() {
+		return goldmoneybalance;
+	}
+	public void setGoldmoneybalance(String goldmoneybalance) {
+		this.goldmoneybalance = goldmoneybalance;
+	}
+	public String getRedmin() {
+		return redmin;
+	}
+	public void setRedmin(String redmin) {
+		this.redmin = redmin;
+	}
+	
+	
+
+}
