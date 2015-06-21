0f46aed503e6e3aaee10b70e8ee00026dd592d4a
406743395 <406743395@qq.com>
2015-06-22 01:48:25 +0800

    fsdfsd

--- src/com/member/form/front/Transform.java
+++ src/com/member/form/front/Transform.java
@@ -4,6 +4,24 @@ public class Transform {
 	
 	private String toUserNumber;
 	private String toUserName;
+	private String payPassword;
+	private String toGoldMoney;
+
+	public String getPayPassword() {
+		return payPassword;
+	}
+
+	public void setPayPassword(String payPassword) {
+		this.payPassword = payPassword;
+	}
+
+	public String getToGoldMoney() {
+		return toGoldMoney;
+	}
+
+	public void setToGoldMoney(String toGoldMoney) {
+		this.toGoldMoney = toGoldMoney;
+	}
 
 	public String getToUserNumber() {
 		return toUserNumber;
