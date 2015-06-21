fd255672df556ee73be06bfce19024f45db54e87
406743395 <406743395@qq.com>
2015-06-21 17:46:13 +0800

    fsdfsd

---
+++ src/com/member/form/front/Transform.java
@@ -0,0 +1,25 @@
+package com.member.form.front;
+
+public class Transform {
+	
+	private String toUserNumber;
+	private String toUserName;
+
+	public String getToUserNumber() {
+		return toUserNumber;
+	}
+
+	public void setToUserNumber(String toUserNumber) {
+		this.toUserNumber = toUserNumber;
+	}
+
+	public String getToUserName() {
+		return toUserName;
+	}
+
+	public void setToUserName(String toUserName) {
+		this.toUserName = toUserName;
+	}
+	
+
+}
