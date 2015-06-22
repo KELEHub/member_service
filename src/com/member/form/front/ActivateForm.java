e91f41ad1b29702a18bda22f3ca01a91e6cd0d1a
406743395 <406743395@qq.com>
2015-06-23 00:01:43 +0800

    fsdfsd

---
+++ src/com/member/form/front/ActivateForm.java
@@ -0,0 +1,71 @@
+package com.member.form.front;
+
+public class ActivateForm {
+	
+	private String number;
+	private String name;
+	private String identity;
+	private String phoneNumber;
+	private String linkAddress;
+	private String recommendNumber;
+	private String registerDate;
+	private String activate;
+	private String flg;
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIdentity() {
+		return identity;
+	}
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getLinkAddress() {
+		return linkAddress;
+	}
+	public void setLinkAddress(String linkAddress) {
+		this.linkAddress = linkAddress;
+	}
+	public String getRecommendNumber() {
+		return recommendNumber;
+	}
+	public void setRecommendNumber(String recommendNumber) {
+		this.recommendNumber = recommendNumber;
+	}
+	public String getRegisterDate() {
+		return registerDate;
+	}
+	public void setRegisterDate(String registerDate) {
+		this.registerDate = registerDate;
+	}
+	public String getActivate() {
+		return activate;
+	}
+	public void setActivate(String activate) {
+		this.activate = activate;
+	}
+	public String getFlg() {
+		return flg;
+	}
+	public void setFlg(String flg) {
+		this.flg = flg;
+	}
+	
+	
+
+}
