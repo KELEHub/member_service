6d5103d987efff39549aabb2a225e4b6e0e106fd
406743395 <406743395@qq.com>
2015-06-22 15:43:58 +0800

    fsdfsd

--- src/com/member/services/front/TransferService.java
+++ src/com/member/services/front/TransferService.java
@@ -1,10 +1,14 @@
 package com.member.services.front;
 
+import java.math.BigDecimal;
+
 import com.member.entity.Information;
 import com.member.entity.SystemParameter;
-import com.member.form.front.Transform;
 
 public interface TransferService {
 
-	public void transferManager(Information from,Information to,Transform form,SystemParameter parameter);
+	public void transferManager(Information from,Information to,BigDecimal goldValue,SystemParameter parameter);
+	
+	public void convertManager(Information info,BigDecimal goldValue,SystemParameter parameter);
+
 }
