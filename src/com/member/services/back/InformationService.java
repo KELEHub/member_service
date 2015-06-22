026accf3e0ef0f7f2c88e14a47464369a2a266d5
406743395 <406743395@qq.com>
2015-06-23 00:02:15 +0800

    fsdfsd

--- src/com/member/services/back/InformationService.java
+++ src/com/member/services/back/InformationService.java
@@ -1,11 +1,21 @@
 package com.member.services.back;
 
+import java.util.List;
+
 import com.member.entity.Information;
+import com.member.entity.Institution;
+import com.member.entity.SystemParameter;
 
 public interface InformationService {
 	
 	public Information getInformationByNumber(String number);
 	
 	public Information getInformationById(Integer id);
+	
+	public List<Information> getInformationForNoActivate(String number);
+	
+	public void deleteData(Information info);
+	
+	public void activate(Information info,Information selfInfo,Institution institution );
 	
 }
