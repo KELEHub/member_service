1de29df7a0a8296213b6bf561671ceba60a7cdea
406743395 <406743395@qq.com>
2015-06-22 15:43:31 +0800

    fsdfsd

---
+++ src/com/member/form/front/ConvertForm.java
@@ -0,0 +1,28 @@
+package com.member.form.front;
+
+public class ConvertForm {
+	private String shoppingMoney;
+	private String toCmrMoney;
+	private String payPassword;
+	public String getShoppingMoney() {
+		return shoppingMoney;
+	}
+	public void setShoppingMoney(String shoppingMoney) {
+		this.shoppingMoney = shoppingMoney;
+	}
+	public String getToCmrMoney() {
+		return toCmrMoney;
+	}
+	public void setToCmrMoney(String toCmrMoney) {
+		this.toCmrMoney = toCmrMoney;
+	}
+	public String getPayPassword() {
+		return payPassword;
+	}
+	public void setPayPassword(String payPassword) {
+		this.payPassword = payPassword;
+	}
+	
+	
+
+}
