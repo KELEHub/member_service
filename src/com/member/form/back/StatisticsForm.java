664e210ef89aca9b7952c2dd0553d299e65fad17
406743395 <406743395@qq.com>
2015-06-27 20:50:12 +0800

    fsdfsd

---
+++ src/com/member/form/back/StatisticsForm.java
@@ -0,0 +1,44 @@
+package com.member.form.back;
+
+public class StatisticsForm {
+	
+	private String dateNumber;
+	private String countBill;
+	private String countRecharge;
+	private String countFromgifts;
+	private String countPointcash;
+	
+	public String getDateNumber() {
+		return dateNumber;
+	}
+	public void setDateNumber(String dateNumber) {
+		this.dateNumber = dateNumber;
+	}
+	public String getCountBill() {
+		return countBill;
+	}
+	public void setCountBill(String countBill) {
+		this.countBill = countBill;
+	}
+	public String getCountRecharge() {
+		return countRecharge;
+	}
+	public void setCountRecharge(String countRecharge) {
+		this.countRecharge = countRecharge;
+	}
+	public String getCountFromgifts() {
+		return countFromgifts;
+	}
+	public void setCountFromgifts(String countFromgifts) {
+		this.countFromgifts = countFromgifts;
+	}
+	public String getCountPointcash() {
+		return countPointcash;
+	}
+	public void setCountPointcash(String countPointcash) {
+		this.countPointcash = countPointcash;
+	}
+	
+	
+
+}
