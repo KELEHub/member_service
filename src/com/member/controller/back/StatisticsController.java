a4621605a666e30b572f7621df78517f6768962e
406743395 <406743395@qq.com>
2015-06-27 20:50:40 +0800

    fsdfsd

---
+++ src/com/member/controller/back/StatisticsController.java
@@ -0,0 +1,51 @@
+package com.member.controller.back;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.member.beans.back.enumData.ProjectEnum;
+import com.member.form.back.StatisticsForm;
+import com.member.services.front.AccountDetailsService;
+
+@Controller
+@RequestMapping(value = "/StatisticsController")
+public class StatisticsController {
+
+	@Resource(name = "AccountDetailsServiceImpl")
+	AccountDetailsService accountDetailsService;
+
+	@RequestMapping(value = "/show", method = RequestMethod.POST)
+	public String show(Model model) {
+		try {
+
+			List<StatisticsForm> list = accountDetailsService
+					.getAccountDetailsGroupBy(ProjectEnum.servicepointsforone,
+							ProjectEnum.recharge, ProjectEnum.fromgifts,
+							ProjectEnum.pointcash);
+			
+			Map<String, Integer> countAll=accountDetailsService.getCountAll(ProjectEnum.servicepointsforone,
+					ProjectEnum.recharge, ProjectEnum.fromgifts,
+					ProjectEnum.pointcash);
+			
+			model.addAttribute("countBill", countAll.get("countBill"));
+			model.addAttribute("countRecharge", countAll.get("countRecharge"));
+			model.addAttribute("countFromgifts", countAll.get("countFromgifts"));
+			model.addAttribute("countPointcash", countAll.get("countPointcash"));
+			model.addAttribute("result", list);
+			return "back/systeminfo/statistics";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "back/systeminfo/statistics";
+		}
+
+	}
+
+}
