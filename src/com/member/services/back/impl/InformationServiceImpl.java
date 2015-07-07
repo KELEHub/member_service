5fce661b61c982e12227886c7ce54d64bea60bf2
TXB <1026800485@qq.com>
2015-07-07 21:40:20 +0800

    更新

--- src/com/member/services/back/impl/InformationServiceImpl.java
+++ src/com/member/services/back/impl/InformationServiceImpl.java
@@ -91,129 +91,142 @@ public class InformationServiceImpl implements InformationService{
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void activate(Information info, Information selfInfo,Institution institution, Information recommendInfo ) {
-		info.setIsActivate(1);
-		info.setActiveDate(new Date());
-		int count =0;
-		if(recommendInfo.getRecommendCount()!=null){
-			count = recommendInfo.getRecommendCount();
-		}
-		recommendInfo.setRecommendCount(count+1);
-		BigDecimal midMoney = selfInfo.getCrmMoney().subtract(new BigDecimal(institution.getRegisterGold()));
-		selfInfo.setCrmMoney(midMoney);
-		AccountDetails acFrom = new AccountDetails();
-		acFrom.setUserNumber(selfInfo.getNumber());
-		acFrom.setCreateTime(new Date());
-		acFrom.setKindData(KindDataEnum.goldmoney);
-		acFrom.setCountNumber(CommonUtil.getCountNumber());
-		
-		/**日期类别统计 */
-		acFrom.setDateNumber(CommonUtil.getDateNumber());
-		acFrom.setProject(ProjectEnum.activateMember);
-		/**积分余额 */
-		acFrom.setPointbalance(selfInfo.getShoppingMoney());
-		/**葛粮币余额 */
-		acFrom.setGoldmoneybalance(midMoney);
-		/**收入 */
-		acFrom.setIncome(new BigDecimal(0));
-		/**支出 */
-		acFrom.setPay(new BigDecimal(institution.getRegisterGold()));
-		/**备注 */
-		acFrom.setRedmin("激活会员，会员号："+info.getNumber());
-		/**用户ID */
-		acFrom.setUserId(selfInfo.getId());
-		institutionDao.saveOrUpdate(info);
-		institutionDao.saveOrUpdate(recommendInfo);
-		institutionDao.saveOrUpdate(acFrom);
-		//礼包生成信息
-		GiftsDetails gf = new GiftsDetails();
-		gf.setUserId(recommendInfo.getId());
-		gf.setChildId(info.getId());
-		gf.setNumber(recommendInfo.getNumber());
-		gf.setGiftEnum(CommonUtil.getGiftEnum());
-		gf.setCountNumber(CommonUtil.getCountNumber());
-		gf.setDateNumber(CommonUtil.getDateNumber());
-		gf.setBatchNo(CommonUtil.getBatchNo());
-		gf.setPointNumber(1);
-		gf.setName("礼包_"+info.getNumber());
-		gf.setCreateTime(new Date());
-		institutionDao.saveOrUpdate(gf);
-		//详细礼包信息
-		int countGifts = 0;
-		if(CommonUtil.getGiftEnum().equals(GiftEnum.TEN)){
-			countGifts = 10;
-		}else{
-			countGifts = 5;
-		}
-		Institution inst = institutionService.getInstitutionInfo();
-		for(int i =1;i<=countGifts;i++){
-			SendGiftsDetails sg = new SendGiftsDetails();
-			sg.setUserId(recommendInfo.getId());
-			sg.setChildId(info.getId());
-			sg.setNumber(recommendInfo.getNumber());
-			sg.setGiftEnum(CommonUtil.getGiftEnum());
-			int countNumber = CommonUtil.getMounthNumber()+i-1;
-			int realNumber = 0;
-			if(countNumber>12){
-				realNumber=(CommonUtil.getYearNumber()+1)*100+(i-(13-CommonUtil.getMounthNumber()));
-			}else{
-				realNumber=CommonUtil.getCountNumber()+i-1;
+		try {
+			info.setIsActivate(1);
+			info.setActiveDate(new Date());
+			int count =0;
+			if(recommendInfo.getRecommendCount()!=null){
+				count = recommendInfo.getRecommendCount();
 			}
-			sg.setCountNumber(realNumber);
-			sg.setDateNumber(CommonUtil.getDateNumber());
-			sg.setBatchNo(CommonUtil.getBatchNo());
-			sg.setPointNumber(1);
-			sg.setName("礼包_"+info.getNumber());
-			sg.setCreateTime(new Date());
-			sg.setPointNumber(i);
-			sg.setGoldMoney(getGoldMoney(inst,i,CommonUtil.getGiftEnum()));
-			sg.setGiftsDetailsId(gf.getId());
-			sg.setCreateTime(new Date());
-			institutionDao.saveOrUpdate(sg);
+			recommendInfo.setRecommendCount(count+1);
+			BigDecimal midMoney = selfInfo.getCrmMoney().subtract(new BigDecimal(institution.getRegisterGold()));
+			selfInfo.setCrmMoney(midMoney);
+			AccountDetails acFrom = new AccountDetails();
+			acFrom.setUserNumber(selfInfo.getNumber());
+			acFrom.setCreateTime(new Date());
+			acFrom.setKindData(KindDataEnum.goldmoney);
+			acFrom.setCountNumber(CommonUtil.getCountNumber());
+			
+			/**日期类别统计 */
+			acFrom.setDateNumber(CommonUtil.getDateNumber());
+			acFrom.setProject(ProjectEnum.activateMember);
+			/**积分余额 */
+			acFrom.setPointbalance(selfInfo.getShoppingMoney());
+			/**葛粮币余额 */
+			acFrom.setGoldmoneybalance(midMoney);
+			/**收入 */
+			acFrom.setIncome(new BigDecimal(0));
+			/**支出 */
+			acFrom.setPay(new BigDecimal(institution.getRegisterGold()));
+			/**备注 */
+			acFrom.setRedmin("激活会员，会员号："+info.getNumber());
+			/**用户ID */
+			acFrom.setUserId(selfInfo.getId());
+			institutionDao.saveOrUpdate(info);
+			institutionDao.saveOrUpdate(recommendInfo);
+			institutionDao.saveOrUpdate(acFrom);
+			//礼包生成信息
+			GiftsDetails gf = new GiftsDetails();
+			gf.setUserId(recommendInfo.getId());
+			gf.setChildId(info.getId());
+			gf.setNumber(recommendInfo.getNumber());
+			gf.setGiftEnum(CommonUtil.getGiftEnum());
+			gf.setCountNumber(CommonUtil.getCountNumber());
+			gf.setDateNumber(CommonUtil.getDateNumber());
+			gf.setBatchNo(CommonUtil.getBatchNo());
+			gf.setPointNumber(1);
+			gf.setName("礼包_"+info.getNumber());
+			gf.setCreateTime(new Date());
+			institutionDao.saveOrUpdate(gf);
+			//详细礼包信息
+			int countGifts = 0;
+			if(CommonUtil.getGiftEnum().equals(GiftEnum.TEN)){
+				countGifts = 10;
+			}else{
+				countGifts = 5;
+			}
+			Institution inst = institutionService.getInstitutionInfo();
+			for(int i =1;i<=countGifts;i++){
+				SendGiftsDetails sg = new SendGiftsDetails();
+				sg.setUserId(recommendInfo.getId());
+				sg.setChildId(info.getId());
+				sg.setNumber(recommendInfo.getNumber());
+				sg.setGiftEnum(CommonUtil.getGiftEnum());
+				int countNumber = CommonUtil.getMounthNumber()+i-1;
+				int realNumber = 0;
+				if(countNumber>12){
+					realNumber=(CommonUtil.getYearNumber()+1)*100+(i-(13-CommonUtil.getMounthNumber()));
+				}else{
+					realNumber=CommonUtil.getCountNumber()+i-1;
+				}
+				sg.setCountNumber(realNumber);
+				sg.setDateNumber(CommonUtil.getDateNumber());
+				sg.setBatchNo(CommonUtil.getBatchNo());
+				sg.setPointNumber(1);
+				sg.setName("礼包_"+info.getNumber());
+				sg.setCreateTime(new Date());
+				sg.setPointNumber(i);
+				sg.setGoldMoney(getGoldMoney(inst,i,CommonUtil.getGiftEnum()));
+				sg.setGiftsDetailsId(gf.getId());
+				sg.setCreateTime(new Date());
+				institutionDao.saveOrUpdate(sg);
+			}
+			//激活人获取50服务积分
+			String hql = "update Information set repeatedMoney=?,repeatedAccumulative=?,shoppingMoney=?,shoppingAccumulative=? where id=?";
+			List<Object> list = new ArrayList<Object>();
+			list.add(selfInfo.getRepeatedMoney().add(new BigDecimal(50)));
+			list.add(selfInfo.getRepeatedAccumulative().add(new BigDecimal(50)));
+			list.add(selfInfo.getShoppingMoney().add(new BigDecimal(50)));
+			list.add(selfInfo.getShoppingAccumulative().add(new BigDecimal(50)));
+			list.add(selfInfo.getId());
+			institutionDao.executeHqlUpdate(hql, list);
+//			selfInfo.setRepeatedMoney();
+//			selfInfo.setRepeatedAccumulative();
+//			selfInfo.setShoppingMoney();
+//			selfInfo.setShoppingAccumulative();
+//			institutionDao.saveOrUpdate(selfInfo);
+			
+			//在AccountDetails表记录激活人获得服务积分明细
+			AccountDetails shopingDetails = new AccountDetails();
+			shopingDetails.setKindData(KindDataEnum.points);
+			/**日期类别统计 */
+			shopingDetails.setDateNumber(CommonUtil.getDateNumber());
+			/**流水号 */
+			shopingDetails.setCountNumber(CommonUtil.getServerCountNumber());
+			/**项目 */
+			shopingDetails.setProject(ProjectEnum.servicepointsforone);
+			/**积分余额 */
+			shopingDetails.setPointbalance(selfInfo.getRepeatedMoney());
+			/**葛粮币余额 */
+			shopingDetails.setGoldmoneybalance(selfInfo.getCrmMoney());
+			/**收入 */
+			shopingDetails.setIncome(new BigDecimal(50));
+			/**支出 */
+			shopingDetails.setPay(new BigDecimal(0));
+			/**备注 */
+			shopingDetails.setRedmin("激活会员");
+			/**用户ID */
+			shopingDetails.setUserId(selfInfo.getId());
+			/**用户登录ID */
+			shopingDetails.setUserNumber(selfInfo.getNumber());
+			shopingDetails.setCreateTime(new Date());
+			institutionDao.saveOrUpdate(shopingDetails);
+			
+			//在RepeatedMoneyStatistics表中添加一条记录，用于发放极差积分
+			RepeatedMoneyStatistics moneyStatistics = new RepeatedMoneyStatistics();
+			moneyStatistics.setCreateTime(new Date());
+			moneyStatistics.setDateNumber(CommonUtil.getDateNumber());
+			moneyStatistics.setDeclarationId(info.getId());
+			moneyStatistics.setDeclarationNumber(info.getNumber());
+			moneyStatistics.setDeclarationBenefitId(selfInfo.getId());
+			moneyStatistics.setDeclarationBenefitNumber(selfInfo.getNumber());
+			moneyStatistics.setSerialNumber(CommonUtil.getCountNumber());
+			moneyStatistics.setState(0);
+			institutionDao.saveOrUpdate(moneyStatistics);
+		} catch (Exception e) {
+			 e.printStackTrace();
 		}
-		//激活人获取50服务积分
-		selfInfo.setRepeatedMoney(selfInfo.getRepeatedMoney().add(new BigDecimal(50)));
-		selfInfo.setRepeatedAccumulative(selfInfo.getRepeatedAccumulative().add(new BigDecimal(50)));
-		selfInfo.setShoppingMoney(selfInfo.getShoppingMoney().add(new BigDecimal(50)));
-		selfInfo.setShoppingAccumulative(selfInfo.getShoppingAccumulative().add(new BigDecimal(50)));
-		institutionDao.saveOrUpdate(selfInfo);
 		
-		//在AccountDetails表记录激活人获得服务积分明细
-		AccountDetails shopingDetails = new AccountDetails();
-		shopingDetails.setKindData(KindDataEnum.points);
-		/**日期类别统计 */
-		shopingDetails.setDateNumber(CommonUtil.getDateNumber());
-		/**流水号 */
-		shopingDetails.setCountNumber(CommonUtil.getServerCountNumber());
-		/**项目 */
-		shopingDetails.setProject(ProjectEnum.servicepointsforone);
-		/**积分余额 */
-		shopingDetails.setPointbalance(selfInfo.getRepeatedMoney());
-		/**葛粮币余额 */
-		shopingDetails.setGoldmoneybalance(selfInfo.getCrmMoney());
-		/**收入 */
-		shopingDetails.setIncome(new BigDecimal(50));
-		/**支出 */
-		shopingDetails.setPay(new BigDecimal(0));
-		/**备注 */
-		shopingDetails.setRedmin("激活会员");
-		/**用户ID */
-		shopingDetails.setUserId(selfInfo.getId());
-		/**用户登录ID */
-		shopingDetails.setUserNumber(selfInfo.getNumber());
-		shopingDetails.setCreateTime(new Date());
-		institutionDao.saveOrUpdate(shopingDetails);
-		
-		//在RepeatedMoneyStatistics表中添加一条记录，用于发放极差积分
-		RepeatedMoneyStatistics moneyStatistics = new RepeatedMoneyStatistics();
-		moneyStatistics.setCreateTime(new Date());
-		moneyStatistics.setDateNumber(CommonUtil.getDateNumber());
-		moneyStatistics.setDeclarationId(info.getId());
-		moneyStatistics.setDeclarationNumber(info.getNumber());
-		moneyStatistics.setDeclarationBenefitId(selfInfo.getId());
-		moneyStatistics.setDeclarationBenefitNumber(selfInfo.getNumber());
-		moneyStatistics.setSerialNumber(CommonUtil.getCountNumber());
-		moneyStatistics.setState(0);
-		institutionDao.saveOrUpdate(moneyStatistics);
 	}
 	
 	private Integer getGoldMoney(Institution inst,int countNumber,GiftEnum gift){
