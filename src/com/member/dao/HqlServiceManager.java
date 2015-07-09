c6afea52c40882dc398118a656216d2d1bc589b4
406743395 <406743395@qq.com>
2015-07-10 00:42:47 +0800

    fsdfsd

--- src/com/member/dao/HqlServiceManager.java
+++ src/com/member/dao/HqlServiceManager.java
@@ -11,9 +11,9 @@ public class HqlServiceManager {
 	public final static String getUserByName=
 		"from NmsUser t where t.userName=?  and t.status>=0";
 	public final static String getRecommendRelation=
-		"from Information where recommendNumber=? and isActivate=1";
+		"from Information where recommendNumber=? and isActivate=1  order by registerDate desc";
 	public final static String getApplyQualification=
-		"from ApplyService where submitNumber=?";
+		"from ApplyService where submitNumber=?  order by applyDate desc";
 	public final static String getServiceByNumber=
 		"from Information where number=?";
 	public final static String getForbidForm=
