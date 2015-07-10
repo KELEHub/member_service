ed7790ed7b85dda1ce30588e6fe980cf0efbe326
406743395 <406743395@qq.com>
2015-07-11 01:34:54 +0800

    fsdfsd

--- src/com/member/controller/back/GiftsDetailsController.java
+++ src/com/member/controller/back/GiftsDetailsController.java
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.member.beans.back.enumData.BatchNoEnum;
 import com.member.beans.back.enumData.GiftEnum;
+import com.member.beans.back.enumData.ProjectEnum;
 import com.member.dao.HqlUserRole;
 import com.member.dao.NmsUserDao;
+import com.member.entity.AccountDetails;
 import com.member.entity.GiftsDetails;
 import com.member.entity.GiftsHistory;
 import com.member.entity.Information;
@@ -27,10 +29,12 @@ import com.member.entity.NmsUser;
 import com.member.entity.SendGiftsDetails;
 import com.member.form.back.GiftsForm;
 import com.member.form.back.GiftsHistoryForm;
+import com.member.form.back.IntegralHistoryForm;
 import com.member.helper.BaseResult;
 import com.member.services.back.GiftsDetailsService;
 import com.member.services.back.InformationService;
 import com.member.services.back.InstitutionService;
+import com.member.services.back.IntegralManagerService;
 import com.member.services.back.ParameterService;
 import com.member.services.back.RoleManageService;
 import com.member.util.CommonUtil;
@@ -38,54 +42,55 @@ import com.member.util.CommonUtil;
 @Controller
 @RequestMapping(value = "/GiftsDetailsController")
 public class GiftsDetailsController {
-	
+
 	@Resource(name = "ParameterServiceImpl")
 	public ParameterService parameterService;
-	
+
 	@Resource(name = "InformationServiceImpl")
 	public InformationService informationService;
-	
+
 	@Resource(name = "NmsUserDaoImpl")
 	private NmsUserDao nmsUserDao;
-	
+
 	@Resource(name = "RoleManageServiceImpl")
 	private RoleManageService roleManageService;
-	
-	
+
 	@Resource(name = "GiftsDetailsServiceImpl")
 	private GiftsDetailsService giftsDetailsService;
-	
+
 	@Resource(name = "InstitutionServiceImpl")
 	public InstitutionService institutionService;
-	
-	
+
+	@Resource(name = "IntegralManagerServiceImpl")
+	public IntegralManagerService integralManagerService;
+
 	@RequestMapping(value = "/show", method = RequestMethod.POST)
 	public String show(Model model) {
 		try {
-		
-			List<GiftsDetails> list = parameterService
-			.getGiftsDetails();
+
+			List<GiftsDetails> list = parameterService.getGiftsDetails();
 			List<GiftsForm> gfList = new ArrayList<GiftsForm>();
-			if(list!=null && list.size()>0){
-				for(GiftsDetails gd:list){
-					Information info =informationService.getInformationById(gd.getChildId());
+			if (list != null && list.size() > 0) {
+				for (GiftsDetails gd : list) {
+					Information info = informationService.getInformationById(gd
+							.getChildId());
 					GiftsForm form = new GiftsForm();
 					form.setCreateTime(gd.getCreateTime().toString());
-					form.setName("礼包_"+info.getNumber());
+					form.setName("礼包_" + info.getNumber());
 					form.setId(gd.getId().toString());
 					int last = 0;
-					if(gd.getGiftEnum().equals(GiftEnum.FIVE)){
-						last=5-gd.getPointNumber()+1;
-					}else{
-						last=10-gd.getPointNumber()+1;
+					if (gd.getGiftEnum().equals(GiftEnum.FIVE)) {
+						last = 5 - gd.getPointNumber() + 1;
+					} else {
+						last = 10 - gd.getPointNumber() + 1;
 					}
-					form.setRemaind("积分领取剩余次数："+last);
+					form.setRemaind("积分领取剩余次数：" + last);
 					form.setNumber(gd.getNumber());
 					gfList.add(form);
 				}
 				model.addAttribute("result", gfList);
 			}
-				return "back/systeminfo/gifts";
+			return "back/systeminfo/gifts";
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -93,72 +98,75 @@ public class GiftsDetailsController {
 		}
 
 	}
-	
-	
-	@RequestMapping(value = "/showDialog",method = RequestMethod.POST)
-	public String showDialog(@RequestBody GiftsForm form,Model model){
-		GiftsDetails giftsDetails = parameterService.getGiftsDetailsById(Integer.valueOf(form.getId()));
-		Information info =informationService.getInformationById(giftsDetails.getChildId());
-		GiftsForm gf =new GiftsForm();
+
+	@RequestMapping(value = "/showDialog", method = RequestMethod.POST)
+	public String showDialog(@RequestBody GiftsForm form, Model model) {
+		GiftsDetails giftsDetails = parameterService
+				.getGiftsDetailsById(Integer.valueOf(form.getId()));
+		Information info = informationService.getInformationById(giftsDetails
+				.getChildId());
+		GiftsForm gf = new GiftsForm();
 		gf.setNumber(giftsDetails.getNumber());
 		gf.setId(giftsDetails.getId().toString());
 		String typeGift = "";
-		if(giftsDetails.getGiftEnum().equals(GiftEnum.FIVE)){
+		if (giftsDetails.getGiftEnum().equals(GiftEnum.FIVE)) {
 			typeGift = "五期次礼包类型";
-		}else{
+		} else {
 			typeGift = "十期次礼包类型";
 		}
 		gf.setTypeGift(typeGift);
-		gf.setName("礼包_"+info.getNumber());
+		gf.setName("礼包_" + info.getNumber());
 		gf.setPointNumber(giftsDetails.getPointNumber().toString());
-		model.addAttribute("bean",gf);
+		model.addAttribute("bean", gf);
 		return "back/systeminfo/editeGifts";
 	}
-	
-	@RequestMapping(value = "/search",method = RequestMethod.POST)
-	public String search(@RequestBody GiftsForm giftsForm,Model model) {
+
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String search(@RequestBody GiftsForm giftsForm, Model model) {
 		try {
 			List<GiftsDetails> list = null;
-			if(giftsForm.getNumber()==null || "".equals(giftsForm.getNumber())){
-				list = parameterService
-				.getGiftsDetails();
-			}else{
-				 list = parameterService
-					.getGiftsDetailsByNumber(giftsForm.getNumber());
+			if (giftsForm.getNumber() == null
+					|| "".equals(giftsForm.getNumber())) {
+				list = parameterService.getGiftsDetails();
+			} else {
+				list = parameterService.getGiftsDetailsByNumber(giftsForm
+						.getNumber());
 			}
-			
+
 			List<GiftsForm> gfList = new ArrayList<GiftsForm>();
-			if(list!=null && list.size()>0){
-				for(GiftsDetails gd:list){
-					Information info =informationService.getInformationById(gd.getChildId());
+			if (list != null && list.size() > 0) {
+				for (GiftsDetails gd : list) {
+					Information info = informationService.getInformationById(gd
+							.getChildId());
 					GiftsForm form = new GiftsForm();
 					form.setCreateTime(gd.getCreateTime().toString());
-					form.setName("礼包_"+info.getNumber());
+					form.setName("礼包_" + info.getNumber());
 					form.setId(gd.getId().toString());
 					int last = 0;
-					if(gd.getGiftEnum().equals(GiftEnum.FIVE)){
-						last=5-gd.getPointNumber()+1;
-					}else{
-						last=10-gd.getPointNumber()+1;
+					if (gd.getGiftEnum().equals(GiftEnum.FIVE)) {
+						last = 5 - gd.getPointNumber() + 1;
+					} else {
+						last = 10 - gd.getPointNumber() + 1;
 					}
-					form.setRemaind("积分领取剩余次数："+last);
+					form.setRemaind("积分领取剩余次数：" + last);
 					form.setNumber(gd.getNumber());
 					gfList.add(form);
 				}
 				model.addAttribute("result", gfList);
 			}
-				return "back/systeminfo/gifts";
+			return "back/systeminfo/gifts";
 
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "back/systeminfo/gifts";
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/set",method = RequestMethod.POST)
+	@RequestMapping(value = "/set", method = RequestMethod.POST)
 	@ResponseBody
-	public BaseResult<Void> set(@RequestBody GiftsForm form,Model model,HttpSession sesison){
+	public BaseResult<Void> set(@RequestBody GiftsForm form, Model model,
+			HttpSession sesison) {
 		BaseResult<Void> result = new BaseResult<Void>();
 		try {
 			Object logonUserO = sesison.getAttribute("logonUser");
@@ -167,48 +175,54 @@ public class GiftsDetailsController {
 			List<NmsUser> users = (List<NmsUser>) nmsUserDao.queryByHql(
 					HqlUserRole.getUserByName, userNaemO);
 			NmsUser user = users.get(0);
-			GiftsDetails giftsDetails = parameterService.getGiftsDetailsById(Integer.valueOf(form.getId()));
-			if(Integer.valueOf(form.getPointNumber()) == 0){
+			GiftsDetails giftsDetails = parameterService
+					.getGiftsDetailsById(Integer.valueOf(form.getId()));
+			if (Integer.valueOf(form.getPointNumber()) == 0) {
 				result.setSuccess(false);
 				result.setMsg("期次号不能等于0");
 				return result;
 			}
-			if(giftsDetails.getGiftEnum().equals(GiftEnum.FIVE)){
-				if(Integer.valueOf(form.getPointNumber())>5){
+			if (giftsDetails.getGiftEnum().equals(GiftEnum.FIVE)) {
+				if (Integer.valueOf(form.getPointNumber()) > 5) {
 					result.setSuccess(false);
 					result.setMsg("期次号必须小于5");
 					return result;
 				}
-			}else{
-				if(Integer.valueOf(form.getPointNumber())>10){
+			} else {
+				if (Integer.valueOf(form.getPointNumber()) > 10) {
 					result.setSuccess(false);
 					result.setMsg("期次号必须小于10");
 					return result;
 				}
 			}
-			if(Integer.valueOf(form.getPointNumber())<giftsDetails.getPointNumber()){
+			if (Integer.valueOf(form.getPointNumber()) < giftsDetails
+					.getPointNumber()) {
 				result.setSuccess(false);
 				result.setMsg("期次号不能小于当前期次号");
 				return result;
 			}
-			if(Integer.valueOf(form.getPointNumber())==giftsDetails.getPointNumber()){
+			if (Integer.valueOf(form.getPointNumber()) == giftsDetails
+					.getPointNumber()) {
 				result.setSuccess(false);
 				result.setMsg("期次号未修改");
 				return result;
 			}
-			Information info =informationService.getInformationById(giftsDetails.getChildId());
-			String remaind = "修改前的期次：" + giftsDetails.getPointNumber()+";"+"修改后的期次："+form.getPointNumber();
+			Information info = informationService
+					.getInformationById(giftsDetails.getChildId());
+			String remaind = "修改前的期次：" + giftsDetails.getPointNumber() + ";"
+					+ "修改后的期次：" + form.getPointNumber();
 			giftsDetails.setPointNumber(Integer.valueOf(form.getPointNumber()));
 			GiftsHistory giftsHistory = new GiftsHistory();
 			giftsHistory.setCreateTime(new Date());
 			giftsHistory.setNumeber(giftsDetails.getNumber());
 			giftsHistory.setUserId(user.getId());
 			giftsHistory.setRemaind(remaind);
-			giftsHistory.setName("礼包_"+info.getNumber());
-			parameterService.saveOrUpdate(giftsDetails, giftsHistory,Integer.valueOf(form.getPointNumber()));
+			giftsHistory.setName("礼包_" + info.getNumber());
+			parameterService.saveOrUpdate(giftsDetails, giftsHistory, Integer
+					.valueOf(form.getPointNumber()));
 			result.setMsg("修改会员详细信息成功.");
 			result.setSuccess(true);
-			
+
 		} catch (Exception e) {
 			result.setSuccess(false);
 			result.setMsg("修改会员详细信息失败，请重新修改.");
@@ -238,10 +252,10 @@ public class GiftsDetailsController {
 				list = parameterService.getGiftsHistoryByOperationId(user
 						.getId());
 			}
-			if (list!= null && list.size() > 0) {
+			if (list != null && list.size() > 0) {
 				for (GiftsHistory eh : list) {
-					List<NmsUser> oprations = (List<NmsUser>) nmsUserDao.queryByHql(
-							HqlUserRole.getUserById, eh.getUserId());
+					List<NmsUser> oprations = (List<NmsUser>) nmsUserDao
+							.queryByHql(HqlUserRole.getUserById, eh.getUserId());
 					NmsUser opration = oprations.get(0);
 					GiftsHistoryForm form = new GiftsHistoryForm();
 					form.setCreateDate(String.valueOf(eh.getCreateTime()));
@@ -258,119 +272,147 @@ public class GiftsDetailsController {
 		return "back/systeminfo/giftsHistory";
 
 	}
-	
-	
+
 	@RequestMapping(value = "/showSend", method = RequestMethod.POST)
 	public String showSend(Model model) {
 		try {
-		
+
 			int countNumber = CommonUtil.getCountNumber();
 			BatchNoEnum batchNo = CommonUtil.getBatchNo();
 			String dayFrom = "";
 			String dayTo = "";
-			if(BatchNoEnum.FIRST.equals(batchNo)){
+			if (BatchNoEnum.FIRST.equals(batchNo)) {
 				dayFrom = "1(31)";
 				dayTo = "10";
-			}else if(BatchNoEnum.SECOND.equals(batchNo)){
+			} else if (BatchNoEnum.SECOND.equals(batchNo)) {
 				dayFrom = "11";
 				dayTo = "20";
-			}else if(BatchNoEnum.THREE.equals(batchNo)){
+			} else if (BatchNoEnum.THREE.equals(batchNo)) {
 				dayFrom = "21";
 				dayTo = "30";
 			}
 			int year = CommonUtil.getYearNumber();
 			int mouth = CommonUtil.getMounthNumber();
-			int nextMoth=0;
+			int nextMoth = 0;
 			int nextYear = 0;
-			String nextDayFrom="";
-			String nextDayTo="";
+			String nextDayFrom = "";
+			String nextDayTo = "";
 			BatchNoEnum nextBatchNo = null;
 			int nextCountNumber = 0;
-		
-			int threeMoth=0;
+
+			int threeMoth = 0;
 			int threeYear = 0;
-			String threeDayFrom="";
-			String threeDayTo="";
+			String threeDayFrom = "";
+			String threeDayTo = "";
 			BatchNoEnum threeBatchNo = null;
 			int threeCountNumber = 0;
-			
-			
-			
-			if(BatchNoEnum.FIRST.equals(batchNo)){
+
+			if (BatchNoEnum.FIRST.equals(batchNo)) {
 				nextBatchNo = BatchNoEnum.SECOND;
-				nextYear =year;
+				nextYear = year;
 				nextMoth = mouth;
 				nextDayFrom = "11";
 				nextDayTo = "20";
-				
+
 				threeBatchNo = BatchNoEnum.THREE;
-				threeYear =year;
+				threeYear = year;
 				threeMoth = mouth;
 				threeDayFrom = "21";
-				threeDayTo= "30";
-			}else if(BatchNoEnum.SECOND.equals(batchNo)){
+				threeDayTo = "30";
+			} else if (BatchNoEnum.SECOND.equals(batchNo)) {
 				nextBatchNo = BatchNoEnum.THREE;
-				nextYear =year;
+				nextYear = year;
 				nextMoth = mouth;
 				nextDayFrom = "21";
 				nextDayTo = "30";
-				
+
 				threeBatchNo = BatchNoEnum.FIRST;
-				if(mouth==12){
-					threeYear =year +1;
+				if (mouth == 12) {
+					threeYear = year + 1;
 					threeMoth = 1;
-				}else{
-					threeYear =year;
-					threeMoth = mouth +1;
+				} else {
+					threeYear = year;
+					threeMoth = mouth + 1;
 				}
 				threeDayFrom = "1(31)";
-				threeDayTo= "10";
-				
-				
-			}else if(BatchNoEnum.THREE.equals(batchNo)){
+				threeDayTo = "10";
+
+			} else if (BatchNoEnum.THREE.equals(batchNo)) {
 				nextBatchNo = BatchNoEnum.FIRST;
-				if(mouth==12){
-					nextYear =year +1;
+				if (mouth == 12) {
+					nextYear = year + 1;
 					nextMoth = 1;
-				}else{
-					nextYear =year;
-					nextMoth = mouth +1;
+				} else {
+					nextYear = year;
+					nextMoth = mouth + 1;
 				}
 				nextDayFrom = "1(31)";
 				nextDayTo = "10";
-				
+
 				threeBatchNo = BatchNoEnum.SECOND;
-				if(mouth==12){
-					threeYear =year +1;
+				if (mouth == 12) {
+					threeYear = year + 1;
 					threeMoth = 1;
-				}else{
-					threeYear =year;
-					threeMoth = mouth +1;
+				} else {
+					threeYear = year;
+					threeMoth = mouth + 1;
 				}
 				threeDayFrom = "11";
-				threeDayTo= "20";
+				threeDayTo = "20";
 			}
-			nextCountNumber= getNextCountNumber(String.valueOf(nextYear),String.valueOf(nextMoth));
-			threeCountNumber= getNextCountNumber(String.valueOf(threeYear),String.valueOf(threeMoth));
-			
-			//当前积分发放总金额
-			String firstGold  =  String.valueOf(mouth) + "月"+dayFrom+"-" +dayTo+"应发放积分";
-			//下批次积分发放总金额
-			String SecondGold =   String.valueOf(nextMoth) + "月"+nextDayFrom+"-" +nextDayTo+"应发放积分";;
-			//下下批次积分发放总金额
-			String threeGold =  String.valueOf(threeMoth) + "月"+threeDayFrom+"-" +threeDayTo+"应发放积分";;
-			 firstGold = firstGold + giftsDetailsService.getCountGoldAll(countNumber, batchNo);
-			 SecondGold = SecondGold + giftsDetailsService.getCountGoldAll(nextCountNumber, nextBatchNo);
-			 threeGold = threeGold + giftsDetailsService.getCountGoldAll(threeCountNumber, threeBatchNo);
-			 model.addAttribute("firstGold", firstGold);
-			 model.addAttribute("SecondGold", SecondGold);
-			 model.addAttribute("threeGold", threeGold);
-//			List<SendGiftsDetails> giftsList = giftsDetailsService.getGiftsDetailsList(countNumber, batchNo);
-//			if(giftsList!=null&&giftsList.size()>0){
-//				model.addAttribute("result", giftsList);
-//				String goldAll = giftsDetailsService.getCountGoldAll(countNumber, batchNo);
-//				model.addAttribute("goldAll", goldAll);
-//			}
+			nextCountNumber = getNextCountNumber(String.valueOf(nextYear),
+					String.valueOf(nextMoth));
+			threeCountNumber = getNextCountNumber(String.valueOf(threeYear),
+					String.valueOf(threeMoth));
+
+			// 当前积分发放总金额
+			String firstGold = String.valueOf(mouth) + "月" + dayFrom + "-"
+					+ dayTo + "应发放积分";
+			// 下批次积分发放总金额
+			String SecondGold = String.valueOf(nextMoth) + "月" + nextDayFrom
+					+ "-" + nextDayTo + "应发放积分";
+			;
+			// 下下批次积分发放总金额
+			String threeGold = String.valueOf(threeMoth) + "月" + threeDayFrom
+					+ "-" + threeDayTo + "应发放积分";
+			;
+			firstGold = firstGold
+					+ giftsDetailsService.getCountGoldAll(countNumber, batchNo);
+			SecondGold = SecondGold
+					+ giftsDetailsService.getCountGoldAll(nextCountNumber,
+							nextBatchNo);
+			threeGold = threeGold
+					+ giftsDetailsService.getCountGoldAll(threeCountNumber,
+							threeBatchNo);
+			model.addAttribute("firstGold", firstGold);
+			model.addAttribute("SecondGold", SecondGold);
+			model.addAttribute("threeGold", threeGold);
+
+			List<AccountDetails> list = integralManagerService
+					.getFromgiftsHistoryPoints();
+			List<IntegralHistoryForm> result = new ArrayList<IntegralHistoryForm>();
+			if (list != null && list.size() > 0) {
+				for (AccountDetails ad : list) {
+					IntegralHistoryForm form = new IntegralHistoryForm();
+					form.setCreateTime(ad.getCreateTime().toString());
+					form.setIncome(ad.getIncome().toString());
+					form.setPay(ad.getPay().toString());
+					form.setPointbalance(ad.getPointbalance().toString());
+					form.setUserNumber(ad.getUserNumber());
+					form.setProject("礼包释放积分");
+					result.add(form);
+				}
+			}
+			model.addAttribute("result", result);
+
+			// List<SendGiftsDetails> giftsList =
+			// giftsDetailsService.getGiftsDetailsList(countNumber, batchNo);
+			// if(giftsList!=null&&giftsList.size()>0){
+			// model.addAttribute("result", giftsList);
+			// String goldAll = giftsDetailsService.getCountGoldAll(countNumber,
+			// batchNo);
+			// model.addAttribute("goldAll", goldAll);
+			// }
 			return "back/systeminfo/giftsSend";
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -378,24 +420,29 @@ public class GiftsDetailsController {
 		}
 
 	}
-	
-	
-	@RequestMapping(value = "/send",method = RequestMethod.POST)
+
+	@RequestMapping(value = "/send", method = RequestMethod.POST)
 	@ResponseBody
-	public BaseResult<Void> send(@RequestBody GiftsForm form,Model model,HttpSession sesison){
+	public BaseResult<Void> send(@RequestBody GiftsForm form, Model model,
+			HttpSession sesison) {
 		BaseResult<Void> result = new BaseResult<Void>();
 		try {
 			int countNumber = CommonUtil.getCountNumber();
 			BatchNoEnum batchNo = CommonUtil.getBatchNo();
-			List<SendGiftsDetails> giftsList = giftsDetailsService.getGiftsDetailsList(countNumber, batchNo);
-			if(giftsList!=null&&giftsList.size()>0){
-				for(SendGiftsDetails ss:giftsList){
+			List<SendGiftsDetails> giftsList = giftsDetailsService
+					.getGiftsDetailsList(countNumber, batchNo);
+			if (giftsList != null && giftsList.size() > 0) {
+				for (SendGiftsDetails ss : giftsList) {
 					giftsDetailsService.senGold(ss);
 				}
+			} else {
+				result.setMsg("没有积分可以释放");
+				result.setSuccess(true);
+				return result;
 			}
 			result.setMsg("积分释放成功");
 			result.setSuccess(true);
-			
+
 		} catch (Exception e) {
 			result.setSuccess(false);
 			result.setMsg("积分释放失败，请重新释放");
@@ -403,14 +450,13 @@ public class GiftsDetailsController {
 		return result;
 	}
 
-	
-	private int getNextCountNumber(String year , String month){
-		if(month.length()<2){
+	private int getNextCountNumber(String year, String month) {
+		if (month.length() < 2) {
 			month = "0" + month;
 		}
-        int result = Integer.valueOf(year+month);
-        return result;
-		
+		int result = Integer.valueOf(year + month);
+		return result;
+
 	}
-	
+
 }
