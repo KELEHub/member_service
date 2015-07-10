e8eee10c3db0f701a1abe56d1d513714e11fbcc7
TXB <1026800485@qq.com>
2015-07-10 22:12:52 +0800

    更新

--- src/com/member/services/back/impl/InstitutionServiceImpl.java
+++ src/com/member/services/back/impl/InstitutionServiceImpl.java
@@ -19,107 +19,103 @@ import com.member.entity.Institution;
 import com.member.form.front.MemberOperForm;
 import com.member.form.front.MemberUpdateForm;
 import com.member.services.back.InstitutionService;
+
+@SuppressWarnings("unchecked")
+
 @Service("InstitutionServiceImpl")
-public class InstitutionServiceImpl implements InstitutionService{
-	
+public class InstitutionServiceImpl implements InstitutionService {
+
 	@Resource(name = "InstitutionDaoImpl")
-    InstitutionDao institutionDao;
-	
+	InstitutionDao institutionDao;
+
 	@Override
-	@SuppressWarnings("unchecked")
 	@Transactional(readOnly = true)
 	public Institution getInstitutionInfo() {
-		List<Institution> dataPermissionCodes=(List<Institution>) institutionDao.queryByHql(HqlInstitution.getInstitutionInfo);
-        if(dataPermissionCodes!=null && dataPermissionCodes.size()>0){
-        	return dataPermissionCodes.get(0);
-        }
-        return null;
+		List<Institution> dataPermissionCodes = (List<Institution>) institutionDao
+				.queryByHql(HqlInstitution.getInstitutionInfo);
+		if (dataPermissionCodes != null && dataPermissionCodes.size() > 0) {
+			return dataPermissionCodes.get(0);
+		}
+		return null;
 	}
 
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public boolean setInstitution(Institution institution) {
-		
 		try {
 			institutionDao.saveOrUpdate(institution);
-			
-	        return true;
+			return true;
 		} catch (Exception e) {
 			return false;
 		}
-		
 	}
-	
+
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public boolean savaOrUpdate(Object ob) {
-		
 		try {
 			institutionDao.saveOrUpdate(ob);
-			
-	        return true;
+			return true;
 		} catch (Exception e) {
 			return false;
 		}
-		
 	}
-	
 
-	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly = true)
 	public List<EditeHistory> getEditeHistoryListByUserId(Integer userId) {
-		String hql="select mr from EditeHistory mr where userId=?";
+		String hql = "select mr from EditeHistory mr where userId=?";
 		List arguments = new ArrayList();
 		arguments.add(userId);
-		List<EditeHistory> result = (List<EditeHistory>)institutionDao.queryByHql(hql,arguments);
-		return result;
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	@Transactional(readOnly = true)
-	public List<EditeHistory> getEditeHistoryList() {
-		String hql="select mr from EditeHistory mr";
-		List<EditeHistory> result = (List<EditeHistory>)institutionDao.queryByHql(hql);
+		List<EditeHistory> result = (List<EditeHistory>) institutionDao
+				.queryByHql(hql, arguments);
 		return result;
 	}
 
-	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional(readOnly = true)
+	public List<EditeHistory> getEditeHistoryList() {
+		String hql = "select mr from EditeHistory mr";
+		List<EditeHistory> result = (List<EditeHistory>) institutionDao
+				.queryByHql(hql);
+		return result;
+	}
+
 	@Override
 	@Transactional(readOnly = true)
 	public Information getNnmuserByName(String number) {
-		String hql="from Information mr where mr.number=?";
+		String hql = "from Information mr where mr.number=?";
 		List arguments = new ArrayList();
 		arguments.add(number);
-		List<Information> result = (List<Information>)institutionDao.queryByHql(hql,arguments);
-		if(result!=null && result.size()>0){
+		List<Information> result = (List<Information>) institutionDao
+				.queryByHql(hql, arguments);
+		if (result != null && result.size() > 0) {
 			return result.get(0);
 		}
 		return null;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly = true)
 	public List<BankService> getBankServiceInfo() {
-		String hql="from BankService mr";
-		List<BankService> result = (List<BankService>)institutionDao.queryByHql(hql);
-		if(result!=null && result.size()>0){
+		String hql = "from BankService mr";
+		List<BankService> result = (List<BankService>) institutionDao
+				.queryByHql(hql);
+		if (result != null && result.size() > 0) {
 			return result;
 		}
 		return null;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly = true)
 	public BankService getBankServiceInfoByName(String name) {
-		String hql="from BankService mr where mr.bankName=?";
+		String hql = "from BankService mr where mr.bankName=?";
 		List arguments = new ArrayList();
 		arguments.add(name);
-		List<BankService> result = (List<BankService>)institutionDao.queryByHql(hql,arguments);
-		if(result!=null && result.size()>0){
+		List<BankService> result = (List<BankService>) institutionDao
+				.queryByHql(hql, arguments);
+		if (result != null && result.size() > 0) {
 			return result.get(0);
 		}
 		return null;
@@ -131,30 +127,28 @@ public class InstitutionServiceImpl implements InstitutionService{
 		try {
 			institutionDao.delete(ob);
 		} catch (Exception e) {
-			
+
 		}
-		
 	}
-	
-	
-	@SuppressWarnings("unchecked")
+
 	@Override
 	@Transactional(readOnly = true)
 	public List<BankRechargeHistory> getBankRechargeHistoryList() {
-		String hql="select mr from BankRechargeHistory mr";
-		List<BankRechargeHistory> result = (List<BankRechargeHistory>)institutionDao.queryByHql(hql);
+		String hql = "select mr from BankRechargeHistory mr";
+		List<BankRechargeHistory> result = (List<BankRechargeHistory>) institutionDao
+				.queryByHql(hql);
 		return result;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly = true)
 	public List<BankRechargeHistory> getBankRechargeHistoryListByUserId(
 			Integer userId) {
-		String hql="select mr from BankRechargeHistory mr where userId=?";
+		String hql = "select mr from BankRechargeHistory mr where userId=?";
 		List arguments = new ArrayList();
 		arguments.add(userId);
-		List<BankRechargeHistory> result = (List<BankRechargeHistory>)institutionDao.queryByHql(hql,arguments);
+		List<BankRechargeHistory> result = (List<BankRechargeHistory>) institutionDao
+				.queryByHql(hql, arguments);
 		return result;
 	}
 
@@ -162,7 +156,7 @@ public class InstitutionServiceImpl implements InstitutionService{
 	@Transactional
 	public void update1Password(MemberOperForm form) {
 		Information ifm = getInformationById(form.getUserId());
-		if(ifm!=null){
+		if (ifm != null) {
 			ifm.setPassword(form.getOnelevelpassword());
 			institutionDao.update(ifm);
 		}
@@ -172,18 +166,19 @@ public class InstitutionServiceImpl implements InstitutionService{
 	@Transactional
 	public void update2Password(MemberOperForm form) {
 		Information ifm = getInformationById(form.getUserId());
-		if(ifm!=null){
+		if (ifm != null) {
 			ifm.setProtectPassword(form.getTwolevelpassword());
 			institutionDao.update(ifm);
 		}
 	}
 
 	public Information getInformationById(Integer id) {
-		String hql="from Information mr where mr.id=?";
-		List<Information> listresult = (List<Information>)institutionDao.queryByHql(hql,id);
-		if(listresult!=null && listresult.size()>0){
+		String hql = "from Information mr where mr.id=?";
+		List<Information> listresult = (List<Information>) institutionDao
+				.queryByHql(hql, id);
+		if (listresult != null && listresult.size() > 0) {
 			return listresult.get(0);
-		}else{
+		} else {
 			return null;
 		}
 	}
@@ -191,46 +186,46 @@ public class InstitutionServiceImpl implements InstitutionService{
 	@Override
 	public void updateMemberInfo(MemberUpdateForm form) {
 		Information ifm = getInformationById(form.getId());
-		if(ifm!=null){
-			/**邮箱 */
+		if (ifm != null) {
+			/** 邮箱 */
 			ifm.setEmail(form.getEmail());
 
-			/**电话号码 */
+			/** 电话号码 */
 			ifm.setPhoneNumber(form.getPhoneNumber());
-			
-			/**银行名称 */
+
+			/** 银行名称 */
 			ifm.setBankName(form.getBankName());
-			
-			/**银行省份 */
+
+			/** 银行省份 */
 			ifm.setBankProvince(form.getBankProvince());
-			
-			/**银行市级*/
+
+			/** 银行市级 */
 			ifm.setBankCity(form.getBankCity());
-			
-			/**银行区县 */
+
+			/** 银行区县 */
 			ifm.setBankCounty(form.getBankCounty());
-			
-			/**银行支行地址 */
+
+			/** 银行支行地址 */
 			ifm.setBankAddress(form.getBankAddress());
-			
-			/**银行卡号 */
+
+			/** 银行卡号 */
 			ifm.setBankCard(form.getBankCard());
-			
-			/**邮编*/
+
+			/** 邮编 */
 			ifm.setPostNumber(form.getPostNumber());
-			
-			/**联系人省份 */
+
+			/** 联系人省份 */
 			ifm.setLinkProvince(form.getLinkProvince());
-			
-			/**联系人市级 */
+
+			/** 联系人市级 */
 			ifm.setLinkCity(form.getLinkCity());
 
-			/**联系人区县 */
+			/** 联系人区县 */
 			ifm.setLinkCounty(form.getLinkCounty());
-			
-			/**联系人地址 */
+
+			/** 联系人地址 */
 			ifm.setLinkAddress(form.getLinkAddress());
-			
+
 			institutionDao.update(ifm);
 		}
 	}
