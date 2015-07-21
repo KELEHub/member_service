728569b80acf4da0545abd9778843b16ed0f1060
TXB <1026800485@qq.com>
2015-07-22 01:04:00 +0800

    fsdfs

--- src/com/member/controller/back/MemberManageController.java
+++ src/com/member/controller/back/MemberManageController.java
@@ -205,7 +205,7 @@ public class MemberManageController {
 		BaseResult<Void> result = new BaseResult<Void>();
 		
 		Information info = informationService.getInformationByNumber(form.getNumber());
-		if(!info.getBankCard().equals(form.getBankCard())){
+		if(!form.getBankCard().equals(info.getBankCard())){
 			if(informationService.countBankCard(form.getBankCard())>=2){
 				result.setMsg("系统已存在两张相同的银行卡，请换卡");
 				result.setSuccess(true);
