65d5793ec1a6e42bb71b7e232c461d3e5083dfc9
TXB <1026800485@qq.com>
2015-07-25 16:57:28 +0800

    fsdfsd

--- src/com/member/dao/HqlServiceManager.java
+++ src/com/member/dao/HqlServiceManager.java
@@ -3,7 +3,7 @@ package com.member.dao;
 public class HqlServiceManager {
 
 	public final static String getService=
-		"from Information where isService=?";
+		"from Information where isService=? order by activeDate desc";
 	public final static String getServiceById=
 		"from Information where id=?";
 	public final static String getApproveService=
