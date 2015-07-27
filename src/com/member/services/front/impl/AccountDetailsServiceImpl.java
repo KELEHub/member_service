a414c0c27d42bce8f958ff226ac25c65d06cd161
TXB <1026800485@qq.com>
2015-07-27 20:43:21 +0800

    fsdfsd

--- src/com/member/services/front/impl/AccountDetailsServiceImpl.java
+++ src/com/member/services/front/impl/AccountDetailsServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.member.beans.back.enumData.ProjectEnum;
 import com.member.dao.ParameterDao;
 import com.member.entity.AccountDetails;
+import com.member.entity.CountService;
 import com.member.form.back.StatisticsForm;
 import com.member.services.front.AccountDetailsService;
 
@@ -40,10 +41,10 @@ public class AccountDetailsServiceImpl implements AccountDetailsService {
 	@Transactional(readOnly = true)
 	@SuppressWarnings("unchecked")
 	public List<AccountDetails> getAccountDetailsByNoservicepoints(
-			String condition, List argumenrs) {
+			String condition, List argumenrs,int pagesize,int pageNumber) {
 		String hql = "from AccountDetails where " + condition;
 		List<AccountDetails> list = (List<AccountDetails>) parameterDao
-				.queryByHql(hql, argumenrs);
+				.queryByHql(hql, pageNumber,pagesize,argumenrs);
 		return list;
 	}
 
@@ -70,6 +71,7 @@ public class AccountDetailsServiceImpl implements AccountDetailsService {
 			ProjectEnum fromgifts, ProjectEnum togoldmoneycut) {
 
 		String hql = "select new map(t.dateNumber as dateNumber,sum(case when t.project =? then 1 else 0 end) as countBill, sum(case when t.project =? then t.income else 0 end) as countRecharge,sum(case when project =? then t.income else 0 end) as countFromgifts,sum(case when project =? then t.pay else 0 end) as countPointcash)from AccountDetails t group by t.dateNumber";
+		String pql = "select new map(t.dateNumber as dateNumber,count(*) as countBill)from CountService t group by t.dateNumber";
 		List arguments = new ArrayList();
 		arguments.add(servicepointsforone);
 		arguments.add(recharge);
@@ -77,17 +79,24 @@ public class AccountDetailsServiceImpl implements AccountDetailsService {
 		arguments.add(togoldmoneycut);
 		List<Object> list = (List<Object>) parameterDao.queryByHql(hql,
 				arguments);
+		List<Object> serviceList = (List<Object>) parameterDao.queryByHql(pql);
 		if (list != null && list.size() > 0) {
 			List<StatisticsForm> rangeList = new ArrayList<StatisticsForm>();
 			for (Object obj : list) {
 				StatisticsForm rif = new StatisticsForm();
 				String dateNumber = ((Map<String, String>) obj)
 						.get("dateNumber");
+				for(Object sb : serviceList){
+					if(dateNumber.equals(((Map<String, String>) sb).get("dateNumber"))){
+						rif.setCountBill(String.valueOf(((Map<String, Integer>)  sb)
+								.get("countBill")));
+						break;
+					}
+				}
 				rif.setDateNumber(dateNumber.substring(0, 4) + "年"
 						+ dateNumber.substring(4, 6) + "月第"
 						+ dateNumber.substring(7, 8) + "期次");
-				rif.setCountBill(String.valueOf(((Map<String, Integer>) obj)
-						.get("countBill")));
+				
 				rif.setCountFromgifts(String
 						.valueOf(((Map<String, Integer>) obj)
 								.get("countFromgifts")));
@@ -127,14 +136,35 @@ public class AccountDetailsServiceImpl implements AccountDetailsService {
 
 	@Override
 	public int getCountServerPointByNumber(String number,String dataNumber) {
-		String hql = " from AccountDetails where project=? and userNumber=? and (dateNumber=? or dateNumber=? or dateNumber=?)";
+		String hql = " from CountService where userNumber=? and (dateNumber=? or dateNumber=? or dateNumber=?)";
 		List arguments = new ArrayList();
-		arguments.add(ProjectEnum.servicepointsforone);
 		arguments.add(number);
 		arguments.add(dataNumber+"01");
 		arguments.add(dataNumber+"02");
 		arguments.add(dataNumber+"03");
 		List<?> list = parameterDao.queryByHql(hql, arguments);
+		if (list != null && list.size() > 0) {
+			return list.size();
+		}
+		return 0;
+	}
+
+	@Override
+	public int couontDetails(String condition, List argumenrs) {
+		String hql = "from AccountDetails where " + condition;
+		List<AccountDetails> list = (List<AccountDetails>) parameterDao
+				.queryByHql(hql, argumenrs);
+		if(list!=null && list.size()>0){
+			return list.size();
+		}
+		return 0;
+	}
+
+	@Override
+	public int countService() {
+		String hql = "from CountService ";
+		List<CountService> list = (List<CountService>) parameterDao
+				.queryByHql(hql);
 		if (list != null && list.size() > 0) {
 			return list.size();
 		}
