7549949f778e5c4fcfcf86d36e4096a33fbd8bdd
TXB <1026800485@qq.com>
2015-07-27 20:41:35 +0800

    fsdfsd

---
+++ src/com/member/services/front/AccountDetailsService.java
@@ -0,0 +1,31 @@
+package com.member.services.front;
+
+import java.util.List;
+import java.util.Map;
+
+import com.member.beans.back.enumData.ProjectEnum;
+import com.member.entity.AccountDetails;
+import com.member.form.back.StatisticsForm;
+
+public interface AccountDetailsService {
+
+	public  List<AccountDetails> getAccountDetailsByUserNumber(String userNumber);
+
+	public List<AccountDetails>  getAccountDetailsByNoservicepoints(String condition,List argumenrs,int pagesize,int pageNumber);
+	
+	public int couontDetails(String condition,List argumenrs);
+	
+	public List<AccountDetails>  getAccountDetailsByservicepoints(String condition,List argumenrs);
+	
+	public List<StatisticsForm>  getAccountDetailsGroupBy(ProjectEnum servicepointsforone,ProjectEnum recharge,ProjectEnum fromgifts,ProjectEnum togoldmoneycut);
+
+	public Map<String, Integer> getCountAll(
+			ProjectEnum servicepointsforone, ProjectEnum recharge,
+			ProjectEnum fromgifts, ProjectEnum togoldmoneycut);
+	
+	public int countService();
+	
+	public int  getCountServerPointByNumber(String number,String countNumber);
+	
+	
+}
